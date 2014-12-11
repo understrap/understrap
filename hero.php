ac9cd33e3c8f1f67bfcfbfddb79b39634facc799
Holger Koenemann <holgerkonemann@holger-konemanns-macbook-pro-10.local>
2014-12-11 18:08:00 +0100

    fix jumbotron

--- hero.php
+++ hero.php
@@ -1,5 +1,13 @@
-<div class="jumbotron">
-  <h1>Hello, world!</h1>
-  <p>At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
-  <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
+<div class="wrapper" id="hero-wrapper">
+    
+    <div class="container">
+        
+        <div class="jumbotron">
+          <h1>Hello, world!</h1>
+          <p>At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+          <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
+        </div>
+        
+    </div>
+    
 </div>
