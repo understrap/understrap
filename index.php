ec81dcebed805ea200c59232ae5bc62c79022d4e
Holger Koenemann <hello@holgerkoenemann.de>
2014-12-22 08:57:31 +0100

    trim auto and manually excerpts
    Displaying the manual and auto excerpt the same way with [...] at the
    end and Bootstrap .btn-default „read more“ link below

--- index.php
+++ index.php
@@ -28,6 +28,7 @@ get_header(); ?>
                     <?php /* Start the Loop */ ?>
                     
                     <?php while ( have_posts() ) : the_post(); ?>
+
                         <?php
                             /* Include the Post-Format-specific template for the content.
                              * If you want to override this in a child theme, then include a file
@@ -35,6 +36,7 @@ get_header(); ?>
                              */
                             get_template_part( 'content', get_post_format() );
                         ?>
+
                     <?php endwhile; ?>
                     
                     <?php understrap_paging_nav(); ?>
