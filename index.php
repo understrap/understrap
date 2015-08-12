a64956956f26eabaab34e17052534908357be933
Holger Könemann <office@holgerkoenemann.de>
2015-08-12 13:39:10 +0200

    check2

--- index.php
+++ index.php
@@ -15,10 +15,6 @@ get_header(); ?>
 
 <?php get_template_part('hero'); ?>
 
-<?php while (have_posts()) : the_post(); ?>
-   <?php if (!is_sticky()) continue; ?>
-        <?php get_template_part('sticky'); ?>
-<?php endwhile; ?>
 
     <div class="wrapper" id="wrapper-index">
         
@@ -27,23 +23,24 @@ get_header(); ?>
 	       <div id="primary" class="<?php if ( is_active_sidebar( 'sidebar-1' ) ) : ?>col-md-8<?php else : ?>col-md-12<?php endif; ?> content-area">
                
                 <main id="main" class="site-main" role="main">
-                 <?php if ( have_posts() ) : ?>
+                
+                <?php if ( have_posts() ) : ?>
+
+                    <?php /* Start the Loop */ ?>
+
+                    <?php while ( have_posts() ) : the_post(); ?>
+
+                            <?php
+                                /* Include the Post-Format-specific template for the content.
+                                 * If you want to override this in a child theme, then include a file
+                                 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+                                 */
+                                get_template_part( 'content', get_post_format() );
+                            ?>
+
+                    <?php endwhile; ?>
                     
-                <?php /* Start the Loop */ ?>
-
-                <?php while ( have_posts() ) : the_post(); ?>
-
-                        <?php
-                            /* Include the Post-Format-specific template for the content.
-                             * If you want to override this in a child theme, then include a file
-                             * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-                             */
-                            get_template_part( 'content', get_post_format() );
-                        ?>
-
-                <?php endwhile; ?>
-                    
-                <?php understrap_paging_nav(); ?>
+                    <?php understrap_paging_nav(); ?>
                     
                 <?php else : ?>
 
