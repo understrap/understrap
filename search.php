0f4f0bcc8763430cd426039e67410bfa65d6528c
koenemann <holger.koenemann@smartsteuer.de>
2016-02-15 09:14:43 +0100

    updating package.json

--- search.php
+++ search.php
@@ -17,7 +17,9 @@ get_header(); ?>
             <?php if ( have_posts() ) : ?>
 
                 <header class="page-header">
+
                     <h1 class="page-title"><?php printf( __( 'Search Results for: %s', 'understrap' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
+                    
                 </header><!-- .page-header -->
 
                 <?php /* Start the Loop */ ?>
