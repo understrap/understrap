cf0f8ed29e35c37c020dab0a5f3a81774ce183f0
Thomas A. Reinert <Thomas-A-Reinert@users.noreply.github.com>
2016-03-03 00:19:13 +0100

    Update index.php
    # tabindex set for better accessibility.
    # Conditional YOAST SEO Breadcrumbs addition.

--- index.php
+++ index.php
@@ -23,7 +23,17 @@ get_header(); ?>
            
 	       <div id="primary" class="<?php if ( is_active_sidebar( 'sidebar-1' ) ) : ?>col-md-8<?php else : ?>col-md-12<?php endif; ?> content-area">
                
-                <main id="main" class="site-main" role="main">
+                <main id="main" class="site-main" role="main" tabindex="-1">
+                	
+                <?php if ( function_exists('yoast_breadcrumb') ) {
+                    $yoast_links_options = get_option( 'wpseo_internallinks' );
+                    $yoast_bc_enabled=$yoast_links_options['breadcrumbs-enable'];
+                        if ($yoast_bc_enabled) { ?>
+                            <div class="breadcrumb">
+                                <?php yoast_breadcrumb('<p id="breadcrumbs"> <i class="fa fa-home"></i> ','</p>'); ?>
+                            </div>
+                    <?php }
+                } ?>
                 
                 <?php if ( have_posts() ) : ?>
 
