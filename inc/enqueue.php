a80dd33a6dd3109bcacbf097668b3ca8ab6c32a2
Holger Könemann <office@holgerkoenemann.de>
2016-04-06 10:45:57 +0200

    some renaming

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -6,9 +6,9 @@
  */
 
 function understrap_scripts() {
-    wp_enqueue_style( 'understrap-theme', get_stylesheet_directory_uri() . '/css/theme.min.css', array(), '0.3.8');
+    wp_enqueue_style( 'understrap-styles', get_stylesheet_directory_uri() . '/css/theme.min.css', array(), '0.3.8');
     wp_enqueue_script('jquery'); 
-    wp_enqueue_script( 'understrap-main', get_template_directory_uri() . '/js/theme.min.js', array(), '0.3.8', true );
+    wp_enqueue_script( 'understrap-scripts', get_template_directory_uri() . '/js/theme.min.js', array(), '0.3.8', true );
 
     if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
         wp_enqueue_script( 'comment-reply' );
