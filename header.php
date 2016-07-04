4620b44c9fdee2def867abfd6343ccd2396c3ce9
Lucas Frezzini <luquiitas.f@gmail.com>
2016-07-04 18:36:14 -0300

    Update X-UA-Compatible content.
    For compatibility in Bootstrap 4 recommended this article: http://stackoverflow.com/a/8942455/6238119 about this, and this is a better method :)

--- header.php
+++ header.php
@@ -10,7 +10,7 @@
 <html <?php language_attributes(); ?>>
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="mobile-web-app-capable" content="yes">
 <meta name="apple-mobile-web-app-capable" content="yes">
