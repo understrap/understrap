9e554e909c821a4f4f1ea07f24b957abbaa145f8
koenemann <holger.koenemann@smartsteuer.de>
2016-07-04 09:56:01 +0200

    Adding version info to footer

--- author.php
+++ author.php
@@ -16,46 +16,46 @@ get_header(); ?>
         
             <div id="primary" class="<?php if ( is_active_sidebar( 'sidebar-1' ) ) : ?>col-md-8<?php else : ?>col-md-12<?php endif; ?> content-area">
                
-                    <main id="main" class="site-main" role="main">
+                <main id="main" class="site-main" role="main">
                         
-                        <header class="page-header author-header">
+                    <header class="page-header author-header">
                         
-                            <?php
+                        <?php
                             $curauth = (isset($_GET['author_name'])) ? get_user_by('slug', $author_name) : get_userdata(intval($author));
-                            ?>
+                        ?>
 
-                            <h1><?php esc_html_e( 'About:', 'understrap' ); ?> <?php echo $curauth->nickname; ?></h1>
+                        <h1><?php esc_html_e( 'About:', 'understrap' ); ?> <?php echo $curauth->nickname; ?></h1>
 
-                            <?php if ( ! empty( $curauth->ID ) ) : ?>
-                                <?php echo get_avatar($curauth->ID); ?>
-                            <?php endif; ?>
+                        <?php if ( ! empty( $curauth->ID ) ) : ?>
+                            <?php echo get_avatar($curauth->ID); ?>
+                        <?php endif; ?>
 
-                            <dl>
-                                <?php if ( ! empty( $curauth->user_url ) ) : ?>
+                        <dl>
+                            <?php if ( ! empty( $curauth->user_url ) ) : ?>
                                 <dt><?php esc_html_e( 'Website', 'understrap' ); ?></dt>
                                 <dd><a href="<?php echo $curauth->user_url; ?>"><?php echo $curauth->user_url; ?></a></dd>
-                                <?php endif; ?>
+                            <?php endif; ?>
 
-                                <?php if ( ! empty( $curauth->user_description ) ) : ?>
+                            <?php if ( ! empty( $curauth->user_description ) ) : ?>
                                 <dt><?php esc_html_e( 'Profile', 'understrap' ); ?></dt>
                                 <dd><?php echo $curauth->user_description; ?></dd>
-                                <?php endif; ?>
-                            </dl>
+                            <?php endif; ?>
+                        </dl>
                         
-                            <h2><?php esc_html_e( 'Posts by', 'understrap' ); ?> <?php echo $curauth->nickname; ?>:</h2>
+                        <h2><?php esc_html_e( 'Posts by', 'understrap' ); ?> <?php echo $curauth->nickname; ?>:</h2>
                             
-                        </header><!-- .page-header -->
+                    </header><!-- .page-header -->
                     
                     <ul>
-                    <!-- The Loop -->
-                    
+                        <!-- The Loop -->
+                        
                         <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-                            <li>
-                                <a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link: <?php the_title(); ?>">
-                                <?php the_title(); ?></a>,
-                                <?php the_time('d M Y'); ?> in <?php the_category('&');?>
+                               <li>
+                                    <a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link: <?php the_title(); ?>">
+                                    <?php the_title(); ?></a>,
+                                    <?php the_time('d M Y'); ?> in <?php the_category('&');?>
                             </li>
-                    
+                        
                         <?php endwhile; ?>
 
                             <?php the_posts_navigation(); ?>
@@ -65,15 +65,16 @@ get_header(); ?>
                             <?php get_template_part( 'loop-templates/content', 'none' ); ?>
 
                         <?php endif; ?>
-                    
-                    <!-- End Loop -->
-    
+                        
+                        <!-- End Loop -->
+        
                     </ul>
+
                 </main><!-- #main -->
                
             </div><!-- #primary -->
 
-        <?php get_sidebar(); ?>
+            <?php get_sidebar(); ?>
 
         </div> <!-- .row -->
         
