1c06c9ecd9cd529a3b8de24d0dac452d164a57a7
Jessica Jean <jessicajsuderno@gmail.com>
2016-09-15 14:42:14 -0700

    Update header.php

--- header.php
+++ header.php
@@ -47,7 +47,7 @@
                                 </button>
 
                                 <!-- Your site title as branding in the menu -->
-                                <a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+                                <a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php if ( !has_custom_logo() ) { bloginfo( 'name' ) } else { the_custom_logo() } ?></a>
 
                             </div>
 
