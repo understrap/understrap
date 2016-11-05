108222b383bad2c54921d823a9d7a9989891e920
Stef Kariotidis <stef.kariotidis@gmail.com>
2016-11-05 14:34:48 +0200

    register left-right templates, WP coding standards

---
+++ sidebar-right.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * The sidebar containing the main widget area.
+ *
+ * @package understrap
+ */
+
+if ( ! is_active_sidebar( 'right-sidebar' ) ) {
+	return;
+}
+// when both sidebars turned one, reduce col size to 3 from 4.
+$sidebar_pos = get_theme_mod( 'understrap_sidebar_position' );
+?>
+
+<?php if ( 'both' === $sidebar_pos ): ?>
+<div class="col-md-3 widget-area" id="secondary" role="complementary">
+	<?php else: ?>
+<div class="col-md-4 widget-area" id="secondary" role="complementary">
+	<?php endif; ?>
+<?php dynamic_sidebar( 'right-sidebar' ); ?>
+
+</div><!-- #secondary -->
