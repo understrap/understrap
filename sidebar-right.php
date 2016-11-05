ca8c7374c054515ad62e485c56b9de3a99ce6abb
Stef Kariotidis <stef.kariotidis@gmail.com>
2016-11-05 14:35:33 +0200

    left and right sidebars

--- sidebar-right.php
+++ sidebar-right.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The sidebar containing the main widget area.
+ * The right sidebar containing the main widget area.
  *
  * @package understrap
  */
@@ -8,14 +8,15 @@
 if ( ! is_active_sidebar( 'right-sidebar' ) ) {
 	return;
 }
-// when both sidebars turned one, reduce col size to 3 from 4.
+
+// when both sidebars turned on reduce col size to 3 from 4.
 $sidebar_pos = get_theme_mod( 'understrap_sidebar_position' );
 ?>
 
 <?php if ( 'both' === $sidebar_pos ): ?>
-<div class="col-md-3 widget-area" id="secondary" role="complementary">
+<div class="col-md-3 widget-area" id="right-sidebar" role="complementary">
 	<?php else: ?>
-<div class="col-md-4 widget-area" id="secondary" role="complementary">
+<div class="col-md-4 widget-area" id="right-sidebar" role="complementary">
 	<?php endif; ?>
 <?php dynamic_sidebar( 'right-sidebar' ); ?>
 
