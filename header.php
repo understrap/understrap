c9265585cbdc7557800db547ba92b206f9fcfe10
Thomas A. Reinert <thomas@tarcgn.de>
2016-11-14 14:35:56 +0100

    Remove "chrome=1" from X-UA-Compatible
    Didn´t validate.

--- header.php
+++ header.php
@@ -11,7 +11,7 @@
 <html <?php language_attributes(); ?>>
 <head>
   <meta charset="<?php bloginfo( 'charset' ); ?>">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="mobile-web-app-capable" content="yes">
   <meta name="apple-mobile-web-app-capable" content="yes">
