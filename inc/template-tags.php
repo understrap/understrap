313856578b7e3b00942257f8e8eb31a024324d7d
Thomas A. Reinert <thomas@tarcgn.de>
2016-11-14 15:20:31 +0100

    Add separator between posted on and updated.

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -15,7 +15,7 @@ if ( ! function_exists( 'understrap_posted_on' ) ) :
 function understrap_posted_on() {
 	$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
 	if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
-		$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">' . __( ' Edited %4$s', 'understrap' ) . '</time>';
+		$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time>, <time class="updated" datetime="%3$s">' . __( ' Edited %4$s', 'understrap' ) . '</time>';
 	}
 
 	$time_string = sprintf( $time_string,
