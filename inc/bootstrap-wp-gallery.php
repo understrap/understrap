b41729840f58c63340980a4568db26d23f3b24ed
Stef Kariotidis <stef.kariotidis@gmail.com>
2016-11-21 18:25:56 +0200

    code formatting

--- inc/bootstrap-wp-gallery.php
+++ inc/bootstrap-wp-gallery.php
@@ -1,105 +1,139 @@
 <?php
-//Based on Roots Sage Gallery: https://github.com/roots/sage/blob/5b9786b8ceecfe717db55666efe5bcf0c9e1801c/lib/gallery.php
+/**
+ * Based on Roots Sage Gallery: https://github.com/roots/sage/blob/5b9786b8ceecfe717db55666efe5bcf0c9e1801c/lib/gallery.php
+ *
+ * @package understrap
+ */
 
-// Remove built in shortcode
-remove_shortcode('gallery', 'gallery_shortcode');
+// Remove built in shortcode.
+remove_shortcode( 'gallery', 'gallery_shortcode' );
 
-// Replace with custom shortcode
-function shortcode_gallery($attr) {
-  $post = get_post();
-  static $instance = 0;
-  $instance++;
-  if (!empty($attr['ids'])) {
-    if (empty($attr['orderby'])) {
-      $attr['orderby'] = 'post__in';
-    }
-    $attr['include'] = $attr['ids'];
-  }
-  $output = apply_filters('post_gallery', '', $attr);
-  if ($output != '') {
-    return $output;
-  }
-  if (isset($attr['orderby'])) {
-    $attr['orderby'] = sanitize_sql_orderby($attr['orderby']);
-    if (!$attr['orderby']) {
-      unset($attr['orderby']);
-    }
-  }
-  extract(shortcode_atts([
-    'order'      => 'ASC',
-    'orderby'    => 'menu_order ID',
-    'id'         => $post->ID,
-    'itemtag'    => '',
-    'icontag'    => '',
-    'captiontag' => '',
-    'columns'    => 3,
-    'size'       => 'thumbnail',
-    'include'    => '',
-    'exclude'    => '',
-    'link'       => ''
-  ], $attr));
-  $id = intval($id);
-  $columns = (12 % $columns == 0) ? $columns : 3;
-  $grid = sprintf('col-sm-%1$s col-lg-%1$s', 12 / $columns);
-  if ($order === 'RAND') {
-    $orderby = 'none';
-  }
-  if (!empty($include)) {
-    $_attachments = get_posts(['include' => $include, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby]);
-    $attachments = [];
-    foreach ($_attachments as $key => $val) {
-      $attachments[$val->ID] = $_attachments[$key];
-    }
-  } elseif (!empty($exclude)) {
-    $attachments = get_children(['post_parent' => $id, 'exclude' => $exclude, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby]);
-  } else {
-    $attachments = get_children(['post_parent' => $id, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby]);
-  }
-  if (empty($attachments)) {
-    return '';
-  }
-  if (is_feed()) {
-    $output = "\n";
-    foreach ($attachments as $att_id => $attachment) {
-      $output .= wp_get_attachment_link($att_id, $size, true) . "\n";
-    }
-    return $output;
-  }
-  $unique = (get_query_var('page')) ? $instance . '-p' . get_query_var('page') : $instance;
-  $output = '<div class="gallery gallery-' . $id . '-' . $unique . '">';
-  $i = 0;
-  foreach ($attachments as $id => $attachment) {
-    switch($link) {
-      case 'file':
-        $image = wp_get_attachment_link($id, $size, false, false);
-        break;
-      case 'none':
-        $image = wp_get_attachment_image($id, $size, false, ['class' => 'thumbnail img-thumbnail']);
-        break;
-      default:
-        $image = wp_get_attachment_link($id, $size, true, false);
-        break;
-    }
-    $output .= ($i % $columns == 0) ? '<div class="row gallery-row">' : '';
-    $output .= '<div class="' . $grid .'">' . $image;
-    if (trim($attachment->post_excerpt)) {
-      $output .= '<div class="caption hidden">' . wptexturize($attachment->post_excerpt) . '</div>';
-    }
-    $output .= '</div>';
-    $i++;
-    $output .= ($i % $columns == 0) ? '</div>' : '';
-  }
-  $output .= ($i % $columns != 0 ) ? '</div>' : '';
-  $output .= '</div>';
-  return $output;
+/*
+ * Replace with custom shortcode
+ */
+function shortcode_gallery( $attr ) {
+	$post = get_post();
+	static $instance = 0;
+	$instance ++;
+	if ( ! empty( $attr['ids'] ) ) {
+		if ( empty( $attr['orderby'] ) ) {
+			$attr['orderby'] = 'post__in';
+		}
+		$attr['include'] = $attr['ids'];
+	}
+	$output = apply_filters( 'post_gallery', '', $attr );
+	if ( $output != '' ) {
+		return $output;
+	}
+	if ( isset( $attr['orderby'] ) ) {
+		$attr['orderby'] = sanitize_sql_orderby( $attr['orderby'] );
+		if ( ! $attr['orderby'] ) {
+			unset( $attr['orderby'] );
+		}
+	}
+	extract( shortcode_atts( [
+		'order'      => 'ASC',
+		'orderby'    => 'menu_order ID',
+		'id'         => $post->ID,
+		'itemtag'    => '',
+		'icontag'    => '',
+		'captiontag' => '',
+		'columns'    => 3,
+		'size'       => 'thumbnail',
+		'include'    => '',
+		'exclude'    => '',
+		'link'       => '',
+	], $attr ) );
+	$id      = intval( $id );
+	$columns = ( 12 % $columns == 0 ) ? $columns : 3;
+	$grid    = sprintf( 'col-sm-%1$s col-lg-%1$s', 12 / $columns );
+	if ( $order === 'RAND' ) {
+		$orderby = 'none';
+	}
+	if ( ! empty( $include ) ) {
+		$_attachments = get_posts( [ 'include'        => $include,
+		                             'post_status'    => 'inherit',
+		                             'post_type'      => 'attachment',
+		                             'post_mime_type' => 'image',
+		                             'order'          => $order,
+		                             'orderby'        => $orderby
+		] );
+		$attachments  = [];
+		foreach ( $_attachments as $key => $val ) {
+			$attachments[ $val->ID ] = $_attachments[ $key ];
+		}
+	} elseif ( ! empty( $exclude ) ) {
+		$attachments = get_children( [ 'post_parent'    => $id,
+		                               'exclude'        => $exclude,
+		                               'post_status'    => 'inherit',
+		                               'post_type'      => 'attachment',
+		                               'post_mime_type' => 'image',
+		                               'order'          => $order,
+		                               'orderby'        => $orderby
+		] );
+	} else {
+		$attachments = get_children( [ 'post_parent'    => $id,
+		                               'post_status'    => 'inherit',
+		                               'post_type'      => 'attachment',
+		                               'post_mime_type' => 'image',
+		                               'order'          => $order,
+		                               'orderby'        => $orderby
+		] );
+	}
+	if ( empty( $attachments ) ) {
+		return '';
+	}
+	if ( is_feed() ) {
+		$output = "\n";
+		foreach ( $attachments as $att_id => $attachment ) {
+			$output .= wp_get_attachment_link( $att_id, $size, true ) . "\n";
+		}
+
+		return $output;
+	}
+	$unique = ( get_query_var( 'page' ) ) ? $instance . '-p' . get_query_var( 'page' ) : $instance;
+	$output = '<div class="gallery gallery-' . $id . '-' . $unique . '">';
+	$i      = 0;
+	foreach ( $attachments as $id => $attachment ) {
+		switch ( $link ) {
+			case 'file':
+				$image = wp_get_attachment_link( $id, $size, false, false );
+				break;
+			case 'none':
+				$image = wp_get_attachment_image( $id, $size, false, [ 'class' => 'thumbnail img-thumbnail' ] );
+				break;
+			default:
+				$image = wp_get_attachment_link( $id, $size, true, false );
+				break;
+		}
+		$output .= ( $i % $columns == 0 ) ? '<div class="row gallery-row">' : '';
+		$output .= '<div class="' . $grid . '">' . $image;
+		if ( trim( $attachment->post_excerpt ) ) {
+			$output .= '<div class="caption hidden">' . wptexturize( $attachment->post_excerpt ) . '</div>';
+		}
+		$output .= '</div>';
+		$i ++;
+		$output .= ( $i % $columns == 0 ) ? '</div>' : '';
+	}
+	$output .= ( $i % $columns != 0 ) ? '</div>' : '';
+	$output .= '</div>';
+
+	return $output;
 }
-add_shortcode('gallery', 'shortcode_gallery');
+
+add_shortcode( 'gallery', 'shortcode_gallery' );
 
 /**
  * Add class="thumbnail img-thumbnail" to attachment items
+ *
+ * @param string $html markup.
+ *
+ * @return mixed
  */
-function attachment_link_class($html) {
-  $html = str_replace('<a', '<a class="thumbnail img-thumbnail"', $html);
-  return $html;
+function attachment_link_class( $html ) {
+	$html = str_replace( '<a', '<a class="thumbnail img-thumbnail"', $html );
+
+	return $html;
 }
-add_filter('wp_get_attachment_link', 'attachment_link_class', 10, 1);
+
+add_filter( 'wp_get_attachment_link', 'attachment_link_class', 10, 1 );
--- inc/customizer.php
+++ inc/customizer.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * understrap Theme Customizer
+ * Understrap Theme Customizer
  *
  * @package understrap
  */
@@ -11,6 +11,11 @@
  * @param WP_Customize_Manager $wp_customize Theme Customizer object.
  */
 if ( ! function_exists( 'understrap_customize_register' ) ) {
+	/**
+	 * Register basic customizer support.
+	 *
+	 * @param object $wp_customize Customizer reference.
+	 */
 	function understrap_customize_register( $wp_customize ) {
 		$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
 		$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
@@ -21,39 +26,44 @@ if ( ! function_exists( 'understrap_customize_register' ) ) {
 add_action( 'customize_register', 'understrap_customize_register' );
 
 if ( ! function_exists( 'understrap_theme_customize_register' ) ) {
+	/**
+	 * Register individual settings through customizer's API.
+	 *
+	 * @param object $wp_customize Customizer reference.
+	 */
 	function understrap_theme_customize_register( $wp_customize ) {
 
 		$wp_customize->add_section( 'understrap_theme_slider_options', array(
-			'title' => __( 'Slider Settings', 'understrap' )
+			'title' => __( 'Slider Settings', 'understrap' ),
 		) );
 
 		$wp_customize->add_setting( 'understrap_theme_slider_count_setting', array(
 			'default'           => '1',
-			'sanitize_callback' => 'absint'
+			'sanitize_callback' => 'absint',
 		) );
 
 		$wp_customize->add_control( 'understrap_theme_slider_count', array(
 			'label'    => __( 'Number of slides displaying at once', 'understrap' ),
 			'section'  => 'understrap_theme_slider_options',
 			'type'     => 'text',
-			'settings' => 'understrap_theme_slider_count_setting'
+			'settings' => 'understrap_theme_slider_count_setting',
 		) );
 
 		$wp_customize->add_setting( 'understrap_theme_slider_time_setting', array(
 			'default'           => '5000',
-			'sanitize_callback' => 'absint'
+			'sanitize_callback' => 'absint',
 		) );
 
 		$wp_customize->add_control( 'understrap_theme_slider_time', array(
 			'label'    => __( 'Slider Time (in ms)', 'understrap' ),
 			'section'  => 'understrap_theme_slider_options',
 			'type'     => 'text',
-			'settings' => 'understrap_theme_slider_time_setting'
+			'settings' => 'understrap_theme_slider_time_setting',
 		) );
 
 		$wp_customize->add_setting( 'understrap_theme_slider_loop_setting', array(
 			'default'           => 'true',
-			'sanitize_callback' => 'esc_textarea'
+			'sanitize_callback' => 'esc_textarea',
 		) );
 
 		$wp_customize->add_control( 'understrap_theme_loop', array(
@@ -64,10 +74,10 @@ if ( ! function_exists( 'understrap_theme_customize_register' ) ) {
 				'true'  => 'yes',
 				'false' => 'no',
 			),
-			'settings' => 'understrap_theme_slider_loop_setting'
+			'settings' => 'understrap_theme_slider_loop_setting',
 		) );
 
-		// Theme layout settings
+		// Theme layout settings.
 		$wp_customize->add_section( 'understrap_theme_layout_options', array(
 			'title'       => __( 'Theme Layout Settings', 'understrap' ),
 			'capability'  => 'edit_theme_options',
@@ -76,10 +86,10 @@ if ( ! function_exists( 'understrap_theme_customize_register' ) ) {
 		) );
 
 		$wp_customize->add_setting( 'understrap_container_type', array(
-			'default'    => 'container',
-			'type'       => 'theme_mod',
+			'default'           => 'container',
+			'type'              => 'theme_mod',
 			'sanitize_callback' => 'esc_textarea',
-			'capability' => 'edit_theme_options'
+			'capability'        => 'edit_theme_options',
 		) );
 
 		$wp_customize->add_control(
@@ -100,10 +110,10 @@ if ( ! function_exists( 'understrap_theme_customize_register' ) ) {
 			) );
 
 		$wp_customize->add_setting( 'understrap_sidebar_position', array(
-			'default'    => 'right',
-			'type'       => 'theme_mod',
+			'default'           => 'right',
+			'type'              => 'theme_mod',
 			'sanitize_callback' => 'esc_textarea',
-			'capability' => 'edit_theme_options',
+			'capability'        => 'edit_theme_options',
 		) );
 
 		$wp_customize->add_control(
@@ -111,7 +121,8 @@ if ( ! function_exists( 'understrap_theme_customize_register' ) ) {
 				$wp_customize,
 				'understrap_sidebar_position', array(
 					'label'       => __( 'Sidebar Positioning', 'understrap' ),
-					'description' => __( "Set sidebar's position. Can either be: right, left, both or none", 'understrap' ),
+					'description' => __( "Set sidebar's position. Can either be: right, left, both or none",
+					'understrap' ),
 					'section'     => 'understrap_theme_layout_options',
 					'settings'    => 'understrap_sidebar_position',
 					'type'        => 'select',
@@ -125,12 +136,12 @@ if ( ! function_exists( 'understrap_theme_customize_register' ) ) {
 				)
 			) );
 
-		// How to display posts index page (home.php)
+		// How to display posts index page (home.php).
 		$wp_customize->add_setting( 'understrap_posts_index_style', array(
-			'default'    => 'default',
-			'type'       => 'theme_mod',
+			'default'           => 'default',
+			'type'              => 'theme_mod',
 			'sanitize_callback' => 'esc_textarea',
-			'capability' => 'edit_theme_options',
+			'capability'        => 'edit_theme_options',
 		) );
 
 		$wp_customize->add_control(
@@ -138,12 +149,12 @@ if ( ! function_exists( 'understrap_theme_customize_register' ) ) {
 				$wp_customize,
 				'understrap_posts_index_style', array(
 					'label'       => __( 'Posts Index Style', 'understrap' ),
-					'description' => __( "Choose how to display latest posts", 'understrap' ),
+					'description' => __( 'Choose how to display latest posts', 'understrap' ),
 					'section'     => 'understrap_theme_layout_options',
 					'settings'    => 'understrap_posts_index_style',
 					'type'        => 'select',
 					'choices'     => array(
-						'default'       => __( 'Default', 'understrap' ),
+						'default' => __( 'Default', 'understrap' ),
 						'masonry' => __( 'Masonry', 'understrap' ),
 					),
 					'priority'    => '30',
@@ -159,7 +170,8 @@ add_action( 'customize_register', 'understrap_theme_customize_register' );
  */
 if ( ! function_exists( 'understrap_customize_preview_js' ) ) {
 	function understrap_customize_preview_js() {
-		wp_enqueue_script( 'understrap_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20130508', true );
+		wp_enqueue_script( 'understrap_customizer', get_template_directory_uri() . '/js/customizer.js',
+			array( 'customize-preview' ), '20130508', true );
 	}
 }
 add_action( 'customize_preview_init', 'understrap_customize_preview_js' );
