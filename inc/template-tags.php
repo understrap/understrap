0fab781b1cc6045951e5135ec3099ceca33ee61b
Stef Kariotidis <stef.kariotidis@gmail.com>
2016-11-21 19:14:49 +0200

    revert escaping variables

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -35,7 +35,7 @@ if ( ! function_exists( 'understrap_posted_on' ) ) :
 			'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
 		);
 
-		echo '<span class="posted-on">' . esc_html( $posted_on ) . '</span><span class="byline"> ' . esc_html( $byline ) . '</span>';
+		echo '<span class="posted-on">' . $posted_on . '</span><span class="byline"> ' . $byline . '</span>';
 
 	}
 endif;
