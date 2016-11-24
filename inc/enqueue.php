a660a7d2f15de1da7c3e623aa99abfba85407ec5
koenemann <office@holgerkoenemann.de>
2016-11-24 12:22:23 +0100

    increase version number in enqueue.php

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -11,9 +11,9 @@ if ( ! function_exists( 'understrap_scripts' ) ) {
 	 */
 	function understrap_scripts() {
 		wp_enqueue_style( 'understrap-styles', get_stylesheet_directory_uri() . '/css/theme.min.css', array(),
-		'0.4.9' );
+		'0.5.0' );
 		wp_enqueue_script( 'jquery' );
-		wp_enqueue_script( 'understrap-scripts', get_template_directory_uri() . '/js/theme.min.js', array(), '0.4.9',
+		wp_enqueue_script( 'understrap-scripts', get_template_directory_uri() . '/js/theme.min.js', array(), '0.5.0',
 		true );
 
 		if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
@@ -31,7 +31,7 @@ if ( ! function_exists( 'understrap_scripts' ) ) {
 		}
 		if ( is_page_template( ( 'page-templates/vertical-one-page.php' ) || is_home() || is_single() ) && $loadit ) {
 			wp_enqueue_script( 'vertical-one-page', get_template_directory_uri() . '/js/vertical-one-page.js',
-			array( 'jquery' ), '0.4.9', true );
+			array( 'jquery' ), '0.5.0', true );
 			$page_for_posts = strtolower( get_the_title( get_option( 'page_for_posts' ) ) );
 			$home_url       = home_url();
 			$is_single      = is_single();
@@ -67,7 +67,7 @@ if ( is_active_sidebar( 'hero' ) ) :
 				);
 
 				wp_enqueue_script( 'understrap-slider-script',
-				get_stylesheet_directory_uri() . '/js/slider_settings.js', array(), '0.4.9' );
+				get_stylesheet_directory_uri() . '/js/slider_settings.js', array(), '0.5.0' );
 				wp_localize_script( 'understrap-slider-script', 'understrap_slider_variables', $data );
 			}
 		}
