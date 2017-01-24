af1bedb6bbffb264ae7cb3a1040226efcc43eba4
Holger Könemann <office@holgerkoenemann.de>
2017-01-24 18:30:57 +0100

    replacing slider

--- sidebar-hero.php
+++ sidebar-hero.php
@@ -11,11 +11,23 @@
 
 	<!-- ******************* The Hero Widget Area ******************* -->
 
-		<div class="owl-carousel">
+		<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  		
+  			<div class="carousel-inner" role="listbox">
 
 			<?php dynamic_sidebar( 'hero' ); ?>
 
+			</div>
+
+			 <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+			    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+			    <span class="sr-only">Previous</span>
+			  </a>
+			  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+			    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+			    <span class="sr-only">Next</span>
+			  </a>
+
 		</div><!-- .owl-carousel -->
 
-		</div><!-- closing owl carousel -->
 <?php endif; ?>
