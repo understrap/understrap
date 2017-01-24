7148f4f7b7e4bb5b017132d9dc373c20cfc1fda9
koenemann <holger.koenemann@smartsteuer.de>
2017-01-24 08:47:53 +0100

    Adding h1 markup to navbar-brand on front/homepage

--- header.php
+++ header.php
@@ -45,10 +45,17 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 					<!-- Your site title as branding in the menu -->
 					<?php if ( ! has_custom_logo() ) { ?>
-					<a class="navbar-brand" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>"
-					   title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>">
-						<?php bloginfo( 'name' ); ?>
-					</a>
+
+						<?php if ( is_front_page() && is_home() ) : ?>
+
+							<h1 class="navbar-brand mb-0"><?php bloginfo( 'name' ); ?></h1>
+							
+						<?php else : ?>
+
+							<a class="navbar-brand" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>"><?php bloginfo( 'name' ); ?></a>
+						
+						<?php endif; ?>
+						
 					
 					<?php } else {
 						the_custom_logo();
