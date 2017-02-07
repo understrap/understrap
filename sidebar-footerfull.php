452f249b2f13364e9d139fb964fed18ee97a3180
koenemann <holger.koenemann@smartsteuer.de>
2017-02-07 15:39:57 +0100

    checking again for duplicate id´s

--- sidebar-footerfull.php
+++ sidebar-footerfull.php
@@ -15,7 +15,7 @@ $container   = get_theme_mod( 'understrap_container_type' );
 
 	<div class="wrapper" id="wrapper-footer-full">
 
-		<div class="<?php echo esc_html( $container ); ?>" id="content" tabindex="-1">
+		<div class="<?php echo esc_html( $container ); ?>" id="footer-full-content" tabindex="-1">
 
 			<div class="row">
 
