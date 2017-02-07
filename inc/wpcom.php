9b43e4b0e4427141164d3357d6f0ce815618bc45
koenemann <holger.koenemann@smartsteuer.de>
2017-02-07 15:30:44 +0100

    cleanup inc/template-tags.php

--- inc/wpcom.php
+++ inc/wpcom.php
@@ -1,31 +1,40 @@
 <?php
 /**
- * WordPress.com-specific functions and definitions.
+ * WordPress.com-specific functions and definitions
  *
  * This file is centrally included from `wp-content/mu-plugins/wpcom-theme-compat.php`.
  *
  * @package understrap
  */
 
-if ( ! function_exists( 'understrap_wpcom_setup' ) ) {
-	/**
-	 * Adds support for wp.com-specific theme functions.
-	 *
-	 * @global array $themecolors Array with theme's colors.
-	 */
-	function understrap_wpcom_setup() {
-		global $themecolors;
+/**
+ * Adds support for wp.com-specific theme functions.
+ *
+ * @global array $themecolors
+ */
+function understrap_wpcom_setup() {
+	global $themecolors;
 
-		// Set theme colors for third party services.
-		if ( ! isset( $themecolors ) ) {
-			$themecolors = array(
-				'bg'     => '',
-				'border' => '',
-				'text'   => '',
-				'link'   => '',
-				'url'    => '',
-			);
-		}
+	// Set theme colors for third party services.
+	if ( ! isset( $themecolors ) ) {
+		$themecolors = array(
+			'bg'     => '',
+			'border' => '',
+			'text'   => '',
+			'link'   => '',
+			'url'    => '',
+		);
 	}
+	
+	/* Add WP.com print styles */
+	add_theme_support( 'print-styles' );
 }
 add_action( 'after_setup_theme', 'understrap_wpcom_setup' );
+
+/*
+ * WordPress.com-specific styles
+ */
+function understrap_wpcom_styles() {
+	wp_enqueue_style( 'understrap-wpcom', get_template_directory_uri() . '/inc/style-wpcom.css', '20160411' );
+}
+add_action( 'wp_enqueue_scripts', 'understrap_wpcom_styles' );
