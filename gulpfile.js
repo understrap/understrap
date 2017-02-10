6ca1abff0813a27bd0b445e12c70dd700844295e
koenemann <holger.koenemann@smartsteuer.de>
2017-02-10 08:53:41 +0100

    Adding /js/ folder to watcher task excluding theme.js and theme.min.js

--- gulpfile.js
+++ gulpfile.js
@@ -103,7 +103,7 @@ gulp.task('sass', function () {
 gulp.task('watch', function () {
     gulp.watch('./sass/**/*.scss', ['sass']);
     gulp.watch('./css/theme.css', ['cssnano']);
-    gulp.watch([basePaths.dev + 'js/**/*.js'], ['scripts'])
+    gulp.watch([basePaths.dev + 'js/**/*.js','js/**/*.js','!js/theme.js','!js/theme.min.js'], ['scripts'])
 });
 
 
