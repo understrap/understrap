355ad5bff4649b9263bc153681cd52b7c571ca5f
Holger Könemann <office@holgerkoenemann.de>
2017-02-11 10:15:59 +0100

    hook in woo commerce

--- inc/woocommerce.php
+++ inc/woocommerce.php
@@ -12,95 +12,26 @@ if ( ! function_exists( 'woocommerce_support' ) ) {
 	 */
 	function woocommerce_support() {
 		add_theme_support( 'woocommerce' );
-		// hook in and customizer form fields.
-		add_filter( 'woocommerce_form_field_args', 'wc_form_field_args', 10, 3 );
 	}
 }
 
 /**
- * Filter hook function monkey patching form classes
- * Author: Adriano Monecchi http://stackoverflow.com/a/36724593/307826
- *
- * @param string $args Form attributes.
- * @param string $key Not in use.
- * @param null   $value Not in use.
- *
- * @return mixed
- */
-function wc_form_field_args( $args, $key, $value = null ) {
+* Remove basic WooCOmmerce hooks.
+*/
+remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
+remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
 
-	// Start field type switch case.
-	switch ( $args['type'] ) {
 
-		/* Targets all select input type elements, except the country and state select input types */
-		case 'select' :
-			// Add a class to the field's html element wrapper - woocommerce
-			// input types (fields) are often wrapped within a <p></p> tag.
-			$args['class'][] = 'form-group';
-			// Add a class to the form input itself.
-			$args['input_class']       = array( 'form-control', 'input-lg' );
-			$args['label_class']       = array( 'control-label' );
-			$args['custom_attributes'] = array(
-				'data-plugin'      => 'select2',
-				'data-allow-clear' => 'true',
-				'aria-hidden'      => 'true',
-				// Add custom data attributes to the form input itself.
-			);
-			break;
+/**
+* Hook in custom WooCommerce content area.
+*/
+add_action('woocommerce_before_main_content', 'my_theme_wrapper_start', 10);
+add_action('woocommerce_after_main_content', 'my_theme_wrapper_end', 10);
 
-		// By default WooCommerce will populate a select with the country names - $args
-		// defined for this specific input type targets only the country select element.
-		case 'country' :
-			$args['class'][]     = 'form-group single-country';
-			$args['label_class'] = array( 'control-label' );
-			break;
+function my_theme_wrapper_start() {
+  echo '<section id="main">';
+}
 
-		// By default WooCommerce will populate a select with state names - $args defined
-		// for this specific input type targets only the country select element.
-		case 'state' :
-			// Add class to the field's html element wrapper.
-			$args['class'][] = 'form-group';
-			// add class to the form input itself.
-			$args['input_class']       = array( '', 'input-lg' );
-			$args['label_class']       = array( 'control-label' );
-			$args['custom_attributes'] = array(
-				'data-plugin'      => 'select2',
-				'data-allow-clear' => 'true',
-				'aria-hidden'      => 'true',
-			);
-			break;
-
-		case 'password' :
-		case 'text' :
-		case 'email' :
-		case 'tel' :
-		case 'number' :
-			$args['class'][]     = 'form-group';
-			$args['input_class'] = array( 'form-control', 'input-lg' );
-			$args['label_class'] = array( 'control-label' );
-			break;
-
-		case 'textarea' :
-			$args['input_class'] = array( 'form-control', 'input-lg' );
-			$args['label_class'] = array( 'control-label' );
-			break;
-
-		case 'checkbox' :
-			$args['label_class'] = array( 'custom-control custom-checkbox' );
-			$args['input_class'] = array( 'custom-control-input', 'input-lg' );
-			break;
-
-		case 'radio' :
-			$args['label_class'] = array( 'custom-control custom-radio' );
-			$args['input_class'] = array( 'custom-control-input', 'input-lg' );
-			break;
-
-		default :
-			$args['class'][]     = 'form-group';
-			$args['input_class'] = array( 'form-control', 'input-lg' );
-			$args['label_class'] = array( 'control-label' );
-			break;
-	} // end switch ($args).
-
-	return $args;
+function my_theme_wrapper_end() {
+  echo '</section>';
 }
