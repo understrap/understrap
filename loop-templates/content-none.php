e81713e7a04df793feef06d26be8ad169e146eba
Jason King <jason@typeplusdesign.com.au>
2017-04-15 21:44:32 +0930

    phpcs fixes.

--- loop-templates/content-none.php
+++ loop-templates/content-none.php
@@ -22,7 +22,11 @@
 		<?php
 		if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
 
-			<p><?php printf( wp_kses( __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'understrap' ), array( 'a' => array( 'href' => array() ) ) ), esc_url( admin_url( 'post-new.php' ) ) ); ?></p>
+			<p><?php printf( wp_kses( __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'understrap' ), array(
+	'a' => array(
+		'href' => array(),
+	),
+) ), esc_url( admin_url( 'post-new.php' ) ) ); ?></p>
 
 		<?php elseif ( is_search() ) : ?>
 
