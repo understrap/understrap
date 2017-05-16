0cd0d3aca4109543a4c42f66fc9961d99a5322f2
koenemann <holger.koenemann@smartsteuer.de>
2017-05-16 09:00:23 +0200

    fixing float-sm-left and right problem with BS´s flex box utilities

--- inc/extras.php
+++ inc/extras.php
@@ -89,25 +89,21 @@ if ( ! function_exists( 'understrap_post_nav' ) ) :
 			return;
 		}
 		?>
-
-		<div class="row">
-			<div class="col-md-12">
-				<nav class="navigation post-navigation">
+				<nav class="container navigation post-navigation">
 					<h2 class="sr-only"><?php _e( 'Post navigation', 'understrap' ); ?></h2>
-					<div class="nav-links">
+					<div class="row nav-links justify-content-between">
 						<?php
 
 							if ( get_previous_post_link() ) {
-								previous_post_link( '<span class="nav-previous float-sm-left">%link</span>', _x( '<i class="fa fa-angle-left"></i>&nbsp;%title', 'Previous post link', 'understrap' ) );
+								previous_post_link( '<span class="nav-previous">%link</span>', _x( '<i class="fa fa-angle-left"></i>&nbsp;%title', 'Previous post link', 'understrap' ) );
 							}
 							if ( get_next_post_link() ) {
-								next_post_link( '<span class="nav-next float-sm-right">%link</span>',     _x( '%title&nbsp;<i class="fa fa-angle-right"></i>', 'Next post link', 'understrap' ) );
+								next_post_link( '<span class="nav-next">%link</span>',     _x( '%title&nbsp;<i class="fa fa-angle-right"></i>', 'Next post link', 'understrap' ) );
 							}
 						?>
 					</div><!-- .nav-links -->
 				</nav><!-- .navigation -->
-			</div>
-		</div>
+
 		<?php
 	}
 endif;
