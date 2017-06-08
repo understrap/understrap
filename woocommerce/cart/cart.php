0a51b721aa6d09ae1a2497fc65900906c56c7c07
Rami Yushuvaev <r_a_m_i@hotmail.com>
2017-06-08 18:22:06 +0300

    Use esc_attr__() insted of __()

--- woocommerce/cart/cart.php
+++ woocommerce/cart/cart.php
@@ -56,7 +56,7 @@ do_action( 'woocommerce_before_cart' ); ?>
 								echo apply_filters( 'woocommerce_cart_item_remove_link', sprintf(
 									'<a href="%s" class="remove" aria-label="%s" data-product_id="%s" data-product_sku="%s">&times;</a>',
 									esc_url( WC()->cart->get_remove_url( $cart_item_key ) ),
-									__( 'Remove this item', 'understrap' ),
+									esc_attr__( 'Remove this item', 'understrap' ),
 									esc_attr( $product_id ),
 									esc_attr( $_product->get_sku() )
 								), $cart_item_key );
--- woocommerce/cart/mini-cart.php
+++ woocommerce/cart/mini-cart.php
@@ -46,7 +46,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 						echo apply_filters( 'woocommerce_cart_item_remove_link', sprintf(
 							'<a href="%s" class="remove" title="%s" data-product_id="%s" data-product_sku="%s">&times;</a>',
 							esc_url( WC()->cart->get_remove_url( $cart_item_key ) ),
-							__( 'Remove this item', 'understrap' ),
+							esc_attr__( 'Remove this item', 'understrap' ),
 							esc_attr( $product_id ),
 							esc_attr( $_product->get_sku() )
 						), $cart_item_key );
