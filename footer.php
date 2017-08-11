df9ebebc851585745ded6e40580f93650b9c5b07
Holger Könemann <office@holgerkoenemann.de>
2017-08-11 20:17:35 +0200

    remove static can script call and replace it with local js

--- footer.php
+++ footer.php
@@ -50,7 +50,6 @@ $container = get_theme_mod( 'understrap_container_type' );
 </div><!-- wrapper end -->
 
 </div><!-- #page -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
 <?php wp_footer(); ?>
 
 </body>
