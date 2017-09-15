10f5e9212e2fb56a86554be67fe8be1a38f73af9
koenemann <holger.koenemann@smartsteuer.de>
2017-09-15 08:09:12 +0200

    Make inc/security.php overwritable
    See: https://github.com/holger1411/understrap/issues/409 - Thx @ErinPo

--- inc/security.php
+++ inc/security.php
@@ -5,15 +5,16 @@
  * @package understrap
  */
 
-/**
- * Removes the generator tag with WP version numbers. Hackers will use this to find weak and old WP installs
- *
- * @return string
- */
-function no_generator() {
-	return '';
-}
-
+if ( ! function_exists( 'no_generator' ) ) {
+	/**
+	 * Removes the generator tag with WP version numbers. Hackers will use this to find weak and old WP installs
+	 *
+	 * @return string
+	 */
+	function no_generator() {
+		return '';
+	}
+} // endif function_exists( 'no_generator' ).
 add_filter( 'the_generator', 'no_generator' );
 
 /*
@@ -28,14 +29,16 @@ remove_action( 'wp_head', 'feed_links_extra', 3 );
 remove_action( 'wp_head', 'adjacent_posts_rel_link_wp_head', 10);
 remove_action( 'wp_head', 'wp_shortlink_wp_head', 10);
 
-/**
- * Show less info to users on failed login for security.
- * (Will not let a valid username be known.)
- *
- * @return string
- */
-function show_less_login_info() {
-	return '<strong>ERROR</strong>: Stop guessing!';
-}
+if ( ! function_exists( 'show_less_login_info' ) ) {
+	/**
+	 * Show less info to users on failed login for security.
+	 * (Will not let a valid username be known.)
+	 *
+	 * @return string
+	 */
+	function show_less_login_info() {
+		return '<strong>ERROR</strong>: Stop guessing!';
+	}
+} // endif function_exists( 'show_less_login_info' ).
 
 add_filter( 'login_errors', 'show_less_login_info' );
