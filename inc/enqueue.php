708751d76d465ca32612462ea72675e00f25f786
koenemann <holger.koenemann@smartsteuer.de>
2017-11-10 14:08:14 +0100

    Testing BS4 Beta 2 Dev version

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -13,8 +13,10 @@ if ( ! function_exists( 'understrap_scripts' ) ) {
 		// Get the theme data.
 		$the_theme = wp_get_theme();
 		wp_enqueue_style( 'understrap-styles', get_stylesheet_directory_uri() . '/css/theme.min.css', array(), $the_theme->get( 'Version' ), false );
-		wp_register_script('jquery-slim', (get_template_directory_uri() . '/js/jquery.slim.min.js'), true, '3.2.1');
-		wp_enqueue_script( 'jquery-slim' );
+		/**
+		* wp_register_script('jquery-slim', (get_template_directory_uri() . '/js/jquery.slim.min.js'), true, '3.2.1');
+		* wp_enqueue_script( 'jquery-slim' );
+		*/
 		wp_enqueue_script( 'popper-scripts', get_template_directory_uri() . '/js/popper.min.js', array(), true);
 		wp_enqueue_script( 'understrap-scripts', get_template_directory_uri() . '/js/theme.min.js', array(), $the_theme->get( 'Version' ), true );
 		if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
