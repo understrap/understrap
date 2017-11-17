49137ed3481ad01e514cfeb501e3b751d803068f
Kevin Goedecke <goedecke@net-studios.de>
2017-11-17 16:19:21 -0600

    Moved assets to vendor folder and removed them from version control

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -14,10 +14,10 @@ if ( ! function_exists( 'understrap_scripts' ) ) {
 		$the_theme = wp_get_theme();
 		wp_enqueue_style( 'understrap-styles', get_stylesheet_directory_uri() . '/css/theme.min.css', array(), $the_theme->get( 'Version' ), false );
 		/**
-		* wp_register_script('jquery-slim', (get_template_directory_uri() . '/js/jquery.slim.min.js'), true, '3.2.1');
+		* wp_register_script('jquery-slim', (get_template_directory_uri() . '/js/vendor/jquery.slim.min.js'), true, '3.2.1');
 		* wp_enqueue_script( 'jquery-slim' );
 		*/
-		wp_enqueue_script( 'popper-scripts', get_template_directory_uri() . '/js/popper.min.js', array(), true);
+		wp_enqueue_script( 'popper-scripts', get_template_directory_uri() . '/js/vendor/popper.min.js', array(), true);
 		wp_enqueue_script( 'understrap-scripts', get_template_directory_uri() . '/js/theme.min.js', array(), $the_theme->get( 'Version' ), true );
 		if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 			wp_enqueue_script( 'comment-reply' );
