5c8b51affec2d63f0db23261e870f233ee165f8c
Vishal Deshpande <deshpande.2004@gmail.com>
2017-11-27 09:06:12 +0530

    Update author.php

--- author.php
+++ author.php
@@ -9,7 +9,6 @@
 
 get_header();
 $container   = get_theme_mod( 'understrap_container_type' );
-$sidebar_pos = get_theme_mod( 'understrap_sidebar_position' );
 ?>
 
 
@@ -88,11 +87,7 @@ $sidebar_pos = get_theme_mod( 'understrap_sidebar_position' );
 		</div><!-- #primary -->
 
 		<!-- Do the right sidebar check -->
-		<?php if ( 'right' === $sidebar_pos || 'both' === $sidebar_pos ) : ?>
-
-			<?php get_sidebar( 'right' ); ?>
-
-		<?php endif; ?>
+		<?php get_template_part( 'global-templates/right-sidebar-check' ); ?>
 
 	</div> <!-- .row -->
 
