bd4c012053042c00fb300ee2d22289ad29b0f951
Vishal Deshpande <deshpande.2004@gmail.com>
2017-11-27 09:01:27 +0530

    Update right-sidebar-check.php

--- global-templates/right-sidebar-check.php
+++ global-templates/right-sidebar-check.php
@@ -1,1 +1,15 @@
+<?php
+/**
+ * Right sidebar check.
+ *
+ * @package understrap
+ */
 
+
+$sidebar_pos = get_theme_mod( 'understrap_sidebar_position' );
+
+if ( 'right' === $sidebar_pos || 'both' === $sidebar_pos ) : 
+
+  get_sidebar( 'right' ); 
+
+endif;
