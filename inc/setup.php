1094e5e6016080c12ead366f8d100697e9b1ed52
Holger Könemann <office@holgerkoenemann.de>
2017-11-29 09:43:44 +0100

    disable theme-settings.php call

--- inc/setup.php
+++ inc/setup.php
@@ -5,7 +5,6 @@
  * @package understrap
  */
 
-require get_template_directory() . '/inc/theme-settings.php';
 
 // Set the content width based on the theme's design and stylesheet.
 if ( ! isset( $content_width ) ) {
