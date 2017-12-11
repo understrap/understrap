a8511cafa41a35eeafa8d5bde217c8a868b4f663
koenemann <holger.koenemann@smartsteuer.de>
2017-12-11 08:44:03 +0100

    Cleanup functions.php

--- functions.php
+++ functions.php
@@ -17,8 +17,6 @@ require get_template_directory() . '/inc/setup.php';
 
 /**
  * Register widget area.
- *
- * @link http://codex.wordpress.org/Function_Reference/register_sidebar
  */
 require get_template_directory() . '/inc/widgets.php';
 
