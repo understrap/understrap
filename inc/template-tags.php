7062beaf610e6ae850b7e843de41f78249cabb95
JOduMonT <jodumont@users.noreply.github.com>
2018-02-01 19:54:47 +0100

    itemprop="author"

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -28,7 +28,7 @@ function understrap_posted_on() {
 	);
 	$byline = sprintf(
 		esc_html_x( 'by %s', 'post author', 'understrap' ),
-		'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
+		'<span class="author vcard" itemprop="author" itemscope itemtype="http://schema.org/Person"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '" rel="me author" itemprop="url">' . esc_html( get_the_author() ) . '</a></span>'
 	);
 	echo '<span class="posted-on">' . $posted_on . '</span><span class="byline"> ' . $byline . '</span>'; // WPCS: XSS OK.
 }
