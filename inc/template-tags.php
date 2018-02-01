da2c3f4d5729e9bc9d4d2f80b7f055718c4d870c
JOduMonT <jodumont@users.noreply.github.com>
2018-02-01 20:38:29 +0100

    add itemprop date + author

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -14,7 +14,7 @@ if ( ! function_exists( 'understrap_posted_on' ) ) :
 function understrap_posted_on() {
 	$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
 	if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
-		$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s"> (%4$s) </time>';
+		$time_string = '<time class="entry-date published" datetime="%1$s" itemprop="datePublished">%2$s</time><time class="updated" datetime="%3$s" itemprop="dateModified"> (%4$s) </time>';
 	}
 	$time_string = sprintf( $time_string,
 		esc_attr( get_the_date( 'c' ) ),
@@ -28,7 +28,7 @@ function understrap_posted_on() {
 	);
 	$byline = sprintf(
 		esc_html_x( 'by %s', 'post author', 'understrap' ),
-		'<span class="author vcard" itemprop="author" itemscope itemtype="http://schema.org/Person"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '" rel="me author" itemprop="url">' . esc_html( get_the_author() ) . '</a></span>'
+		'<span class="author vcard" itemprop="author editor publisher" itemscope itemtype="http://schema.org/Person"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '" rel="me author" itemprop="url"><span itemprop="name">' . esc_html( get_the_author() ) . '</span></a></span>'
 	);
 	echo '<span class="posted-on">' . $posted_on . '</span><span class="byline"> ' . $byline . '</span>'; // WPCS: XSS OK.
 }
