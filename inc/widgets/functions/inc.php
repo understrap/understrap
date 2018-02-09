ae2bf5830535aca3bef5c5ef5cfd84bbc85b85a5
LeanOperation <LeanOperation@github.com>
2018-02-08 16:34:12 -0800

    Navigation Widget

--- inc/widgets/functions/inc.php
+++ inc/widgets/functions/inc.php
@@ -5,5 +5,4 @@ require get_template_directory() . '/inc/widgets/functions/archive.php';
 require get_template_directory() . '/inc/widgets/functions/class-walker-category.php';
 require get_template_directory() . '/inc/widgets/functions/pages.php';
 require get_template_directory() . '/inc/widgets/functions/class-walker-page.php';
-
- 
+require get_template_directory() . '/inc/widgets/functions/class-widget-nav-walker.php';
