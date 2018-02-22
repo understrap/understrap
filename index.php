7230dd4fdd2df3643a6c9dc914cc4d22e68aaf30
Holger Könemann <office@holgerkoenemann.de>
2018-02-22 18:27:55 +0100

    See https://github.com/holger1411/understrap/issues/565

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ $container   = get_theme_mod( 'understrap_container_type' );
 	<?php get_template_part( 'global-templates/hero' ); ?>
 <?php endif; ?>
 
-<div class="wrapper" id="wrapper-index">
+<div class="wrapper" id="index-wrapper">
 
 	<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
 
