4a89b37500e0007b740acfa341d75264adac692f
IngoVals <ingovals@gmail.com>
2018-03-03 19:09:49 +0000

    Updated the markup for input groups according to BS 4 changes

--- searchform.php
+++ searchform.php
@@ -11,7 +11,7 @@
 	<div class="input-group">
 		<input class="field form-control" id="s" name="s" type="text"
 			placeholder="<?php esc_attr_e( 'Search &hellip;', 'understrap' ); ?>" value="<?php the_search_query(); ?>">
-		<span class="input-group-btn">
+		<span class="input-group-append">
 			<input class="submit btn btn-primary" id="searchsubmit" name="submit" type="submit"
 			value="<?php esc_attr_e( 'Search', 'understrap' ); ?>">
 	</span>
--- woocommerce/product-searchform.php
+++ woocommerce/product-searchform.php
@@ -26,7 +26,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 		<input type="search" id="woocommerce-product-search-field-<?php echo isset( $index ) ? absint( $index ) : 0; ?>" class="search-field field form-control" placeholder="<?php echo esc_attr__( 'Search products&hellip;', 'understrap' ); ?>" value="<?php echo get_search_query(); ?>" name="s" />
 		<input type="hidden" name="post_type" value="product" />
-		<span class="input-group-btn">
+		<span class="input-group-append">
 			<input class="submit btn btn-primary" type="submit" value="<?php echo esc_attr_x( 'Search', 'submit button', 'understrap' ); ?>" />
 			</span>
 	</div>
