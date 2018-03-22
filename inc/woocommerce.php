a2fa9e56ba0326ed36297d699ca06ada26110107
Holger Könemann <office@holgerkoenemann.de>
2018-03-22 18:47:51 +0100

    Remove woocommerce.php and replace it with hooked version
    See
    https://github.com/understrap/understrap/issues/606#issuecomment-3753920
    87
    
    Thx @ZacharyElkins !

--- inc/woocommerce.php
+++ inc/woocommerce.php
@@ -21,6 +21,39 @@ if ( ! function_exists( 'understrap_woocommerce_support' ) ) {
 		add_filter( 'woocommerce_form_field_args', 'understrap_wc_form_field_args', 10, 3 );
 	}
 }
+
+/**
+* First unhook the WooCommerce wrappers
+*/
+remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
+remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
+
+/**
+* Then hook in your own functions to display the wrappers your theme requires
+*/
+add_action('woocommerce_before_main_content', 'understrap_woocommerce_wrapper_start', 10);
+add_action('woocommerce_after_main_content', 'understrap_woocommerce_wrapper_end', 10);
+if ( ! function_exists( 'understrap_woocommerce_wrapper_start' ) ) {
+	function understrap_woocommerce_wrapper_start() {
+		$container   = get_theme_mod( 'understrap_container_type' );
+		echo '<div class="wrapper" id="woocommerce-wrapper">';
+	  echo '<div class="' . esc_attr( $container ) . '" id="content" tabindex="-1">';
+		echo '<div class="row">';
+		get_template_part( 'global-templates/left-sidebar-check' );
+		echo '<main class="site-main" id="main">';
+	}
+}
+if ( ! function_exists( 'understrap_woocommerce_wrapper_end' ) ) {
+function understrap_woocommerce_wrapper_end() {
+	echo '</main><!-- #main -->';
+	echo '</div><!-- #primary -->';
+	get_template_part( 'global-templates/right-sidebar-check' );
+  echo '</div><!-- .row -->';
+	echo '</div><!-- Container end -->';
+	echo '</div><!-- Wrapper end -->';
+	}
+}
+
 /**
  * Filter hook function monkey patching form classes
  * Author: Adriano Monecchi http://stackoverflow.com/a/36724593/307826
@@ -98,3 +131,4 @@ function understrap_wc_form_field_args( $args, $key, $value = null ) {
 	} // end switch ($args).
 	return $args;
 }
+
