89a7860da06d6436e9088fba8a6a36cdd2d4a492
Unknown <zach@zacharyelkins.com>
2018-03-22 13:58:07 -0600

    More formatting
    Identical to Woo files;

--- woocommerce/cart/cart.php
+++ woocommerce/cart/cart.php
@@ -64,9 +64,8 @@ do_action( 'woocommerce_before_cart' ); ?>
 							?>
 						</td>
 
-						<td class="product-thumbnail">
-							<?php
-								$thumbnail = apply_filters( 'woocommerce_cart_item_thumbnail', $_product->get_image(), $cart_item, $cart_item_key );
+						<td class="product-thumbnail"><?php
+						$thumbnail = apply_filters( 'woocommerce_cart_item_thumbnail', $_product->get_image(), $cart_item, $cart_item_key );
 
 								if ( ! $product_permalink ) {
 									echo $thumbnail;
