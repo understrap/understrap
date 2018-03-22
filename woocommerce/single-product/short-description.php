0ee0b8a44fa36a8e750f0eff67b2b7658f329799
Unknown <zach@zacharyelkins.com>
2018-03-22 14:41:54 -0600

    short-description check
    Apply woocommerce_short_description filter before check if excerpt is empty;
    
    Per new Woo template files;

--- woocommerce/single-product/short-description.php
+++ woocommerce/single-product/short-description.php
@@ -22,11 +22,13 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 global $post;
 
-if ( ! $post->post_excerpt ) {
+$short_description = apply_filters( 'woocommerce_short_description', $post->post_excerpt );
+
+if ( ! $short_description ) {
 	return;
 }
 
 ?>
-<div itemprop="description" class="lead">
-	<?php echo apply_filters( 'woocommerce_short_description', $post->post_excerpt ) ?>
+<div class="woocommerce-product-details__short-description">
+	<?php echo $short_description; // WPCS: XSS ok. ?>
 </div>
