395c4da9f7aa9f56ca1afb85576e44c3d6cb6d65
Unknown <zach@zacharyelkins.com>
2018-03-23 12:42:33 -0600

    Add Bootstrap button classes to simple.php
    Replace Woo classes with Bootstrap classes;
    
    *Understrap edit

--- woocommerce/single-product/add-to-cart/simple.php
+++ woocommerce/single-product/add-to-cart/simple.php
@@ -55,7 +55,7 @@ if ( $product->is_in_stock() ) : ?>
 			do_action( 'woocommerce_after_add_to_cart_quantity' );
 		?>
 
-		<button type="submit" name="add-to-cart" value="<?php echo esc_attr( $product->get_id() ); ?>" class="single_add_to_cart_button button alt"><?php echo esc_html( $product->single_add_to_cart_text() ); ?></button>
+		<button type="submit" name="add-to-cart" value="<?php echo esc_attr( $product->get_id() ); ?>" class="btn btn-outline-primary"><?php echo esc_html( $product->single_add_to_cart_text() ); ?></button>
 
 		<?php
 			/**
