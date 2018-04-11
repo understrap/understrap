2743321cd12eb00f4d02b672a2765132ab6b07bb
Holger Könemann <office@holgerkoenemann.de>
2018-04-11 21:05:08 +0200

    define a nicer navbar bg type

--- header.php
+++ header.php
@@ -32,7 +32,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 		<a class="skip-link screen-reader-text sr-only" href="#content"><?php esc_html_e( 'Skip to content', 'understrap' ); ?></a>
 
-		<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+		<nav class="navbar navbar-expand-md navbar-dark bg-primary">
 
 		<?php if ( 'container' == $container ) : ?>
 			<div class="container" >
