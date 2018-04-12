548f4f04c1947da6e5238d8a17309e50e8bf8789
Holger Könemann <office@holgerkoenemann.de>
2018-04-12 20:35:08 +0200

    Moving closing primary </div> into right-sidebar-check.php for more clearness - Thx @jack-fdrv

--- global-templates/right-sidebar-check.php
+++ global-templates/right-sidebar-check.php
@@ -6,6 +6,8 @@
  */
 ?>
 
+</div><!-- #closing the primary container from /global-templates/left-sidebar-check.php -->
+
 <?php $sidebar_pos = get_theme_mod( 'understrap_sidebar_position' ); ?>
 
 <?php if ( 'right' === $sidebar_pos || 'both' === $sidebar_pos ) : ?>
