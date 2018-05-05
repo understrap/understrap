524418e20469a4947f352af13794610401854d1d
Johan Nielsen <odinp123@gmail.com>
2018-05-05 09:47:03 +0200

    formatting and file check and type0

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -11,12 +11,12 @@
  */
 function asset_path($filename) {
     $manifest_path = get_stylesheet_directory_uri() .'/rev-manifest.json';
-    if (isset($manifest_path)) {
-        $manifest = json_decode(file_get_contents($manifest_path), TRUE);
+    if ( file_exists($manifest_path ) ) {
+        $manifest = json_decode( file_get_contents( $manifest_path ), TRUE );
     } else {
         $manifest = [];
     }
-    if (array_key_exists($filename, $manifest)) {
+    if ( array_key_exists( $filename, $manifest ) ) {
         return $manifest[$filename];
     }
     return $filename;
@@ -28,12 +28,12 @@ if ( ! function_exists( 'understrap_scripts' ) ) {
 	 */
 	function understrap_scripts() {
 
-		wp_enqueue_style( 'understrap-styles', get_stylesheet_directory_uri() . '/'. asset_path('css/child-theme.min.css'), array(), null);
+		wp_enqueue_style( 'understrap-styles', get_stylesheet_directory_uri() . '/'. asset_path('css/theme.min.css'), array(), null);
 
 		wp_enqueue_script( 'jquery');
 		wp_enqueue_script( 'popper-scripts', get_template_directory_uri() . '/js/popper.min.js', array(), false, true);
 
-		wp_enqueue_script( 'understrap-scripts', get_template_directory_uri() . '/' . asset_path('js/child-theme.min.js'), array(), null, true );
+		wp_enqueue_script( 'understrap-scripts', get_template_directory_uri() . '/' . asset_path('js/theme.min.js'), array(), null, true );
 		if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 			wp_enqueue_script( 'comment-reply' );
 		}
