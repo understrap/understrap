9407ee0d96d4da71b599676ae6056778b450d238
KoenemannH <holger.koenemann@smartsteuer.de>
2018-07-24 15:27:43 +0200

    See https://github.com/understrap/understrap/issues/750
    Thx @DDfakt

--- inc/jetpack.php
+++ inc/jetpack.php
@@ -21,7 +21,7 @@ if ( ! function_exists ( 'understrap_components_jetpack_setup' ) ) {
 		// Add theme support for Infinite Scroll.
 		add_theme_support( 'infinite-scroll', array(
 			'container' => 'main',
-			'render'    => 'components_infinite_scroll_render',
+			'render'    => 'understrap_components_infinite_scroll_render',
 			'footer'    => 'page',
 		) );
 
