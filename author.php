c038da308a0b3a180c9b609b1238ab63449a2c43
IanDelMar <42134098+IanDelMar@users.noreply.github.com>
2018-11-18 23:45:32 +0100

    Fix space usage, indentation, readability

--- author.php
+++ author.php
@@ -12,9 +12,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 get_header();
-$container   = get_theme_mod( 'understrap_container_type' );
+$container = get_theme_mod( 'understrap_container_type' );
 ?>
-
 
 <div class="wrapper" id="author-wrapper">
 
@@ -30,32 +29,36 @@ $container   = get_theme_mod( 'understrap_container_type' );
 				<header class="page-header author-header">
 
 					<?php
-					$curauth = ( isset( $_GET['author_name'] ) ) ? get_user_by( 'slug',
-						$author_name ) : get_userdata( intval( $author ) );
+					if ( isset( $_GET['author_name'] ) ) {
+						$curauth = get_user_by( 'slug', $author_name );
+					} else {
+						$curauth = get_userdata( intval( $author ) );
+					}
 					?>
 
-					<h1><?php esc_html_e( 'About:', 'understrap' ); ?><?php echo esc_html( $curauth->nickname ); ?></h1>
+					<h1><?php echo esc_html__( 'About:', 'understrap' ) . ' ' . esc_html( $curauth->nickname ); ?></h1>
 
 					<?php if ( ! empty( $curauth->ID ) ) : ?>
 						<?php echo get_avatar( $curauth->ID ); ?>
 					<?php endif; ?>
 
-					<dl>
-						<?php if ( ! empty( $curauth->user_url ) ) : ?>
-							<dt><?php esc_html_e( 'Website', 'understrap' ); ?></dt>
-							<dd>
-								<a href="<?php echo esc_url( $curauth->user_url ); ?>"><?php echo esc_html( $curauth->user_url ); ?></a>
-							</dd>
-						<?php endif; ?>
+					<?php if ( ! empty( $curauth->user_url ) || ! empty( $curauth->user_description ) ) : ?>
+						<dl>
+							<?php if ( ! empty( $curauth->user_url ) ) : ?>
+								<dt><?php esc_html_e( 'Website', 'understrap' ); ?></dt>
+								<dd>
+									<a href="<?php echo esc_url( $curauth->user_url ); ?>"><?php echo esc_html( $curauth->user_url ); ?></a>
+								</dd>
+							<?php endif; ?>
 
-						<?php if ( ! empty( $curauth->user_description ) ) : ?>
-							<dt><?php esc_html_e( 'Profile', 'understrap' ); ?></dt>
-							<dd><?php echo esc_html( $curauth->user_description ); ?></dd>
-						<?php endif; ?>
-					</dl>
+							<?php if ( ! empty( $curauth->user_description ) ) : ?>
+								<dt><?php esc_html_e( 'Profile', 'understrap' ); ?></dt>
+								<dd><?php esc_html_e( $curauth->user_description ); ?></dd>
+							<?php endif; ?>
+						</dl>
+					<?php endif; ?>
 
-					<h2><?php esc_html_e( 'Posts by', 'understrap' ); ?> <?php echo esc_html( $curauth->nickname ); ?>
-						:</h2>
+					<h2><?php echo esc_html( 'Posts by', 'understrap' ) . ' ' . esc_html( $curauth->nickname ); ?>:</h2>
 
 				</header><!-- .page-header -->
 
@@ -65,11 +68,17 @@ $container   = get_theme_mod( 'understrap_container_type' );
 					<?php if ( have_posts() ) : ?>
 						<?php while ( have_posts() ) : the_post(); ?>
 							<li>
-								<a rel="bookmark" href="<?php the_permalink() ?>"
-								   title="<?php esc_html_e( 'Permanent Link:', 'understrap' ); ?> <?php the_title(); ?>">
-									<?php the_title(); ?></a>,
-								<?php understrap_posted_on(); ?> <?php esc_html_e( 'in',
-								'understrap' ); ?> <?php the_category( '&' ); ?>
+								<?php
+								printf(
+									'<a rel="bookmark" href="%1$s" title="%2$s %3$s">%3$s</a>',
+									esc_url( apply_filters( 'the_permalink', get_permalink( $post ), $post ) ),
+									esc_attr( __( 'Permanent Link:', 'understrap' ) ),
+									the_title( '', '', false )
+								);
+								?>
+								<?php understrap_posted_on(); ?> 
+								<?php esc_html_e( 'in', 'understrap' ); ?> 
+								<?php the_category( '&' ); ?>
 							</li>
 						<?php endwhile; ?>
 
@@ -88,13 +97,13 @@ $container   = get_theme_mod( 'understrap_container_type' );
 			<!-- The pagination component -->
 			<?php understrap_pagination(); ?>
 
-		<!-- Do the right sidebar check -->
-		<?php get_template_part( 'global-templates/right-sidebar-check' ); ?>
+			<!-- Do the right sidebar check -->
+			<?php get_template_part( 'global-templates/right-sidebar-check' ); ?>
 
-	</div> <!-- .row -->
+		</div> <!-- .row -->
 
-</div><!-- Container end -->
+	</div><!-- #content -->
 
-</div><!-- Wrapper end -->
+</div><!-- #author-wrapper -->
 
 <?php get_footer(); ?>
