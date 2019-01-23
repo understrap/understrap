111fc9ab41f8d60e01038b05c1b65bd6f3dd4360
Craig Wayne <craigiswayne@gmail.com>
2019-01-23 19:51:24 +0200

    Child Theme Support for `/css/theme.min.css`
    When this theme is used as a parent theme, the link to `theme.min.css` references a uri in the child theme. 
    Unless one is created, it will result it broken styles.

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -19,7 +19,7 @@ if ( ! function_exists( 'understrap_scripts' ) ) {
 		$theme_version = $the_theme->get( 'Version' );
 
 		$css_version = $theme_version . '.' . filemtime( get_template_directory() . '/css/theme.min.css' );
-		wp_enqueue_style( 'understrap-styles', get_stylesheet_directory_uri() . '/css/theme.min.css', array(), $css_version );
+		wp_enqueue_style( 'understrap-styles', get_template_directory_uri() . '/css/theme.min.css', array(), $css_version );
 
 		wp_enqueue_script( 'jquery' );
 
