fa0bef5e7252cc6681c8334edc1a5f40cffe5409
KoenemannH <holger.koenemann@smartsteuer.de>
2019-03-20 16:20:23 +0100

    Fixing problem with tiny col on mobile

--- woocommerce/myaccount/form-login.php
+++ woocommerce/myaccount/form-login.php
@@ -25,7 +25,7 @@ do_action( 'woocommerce_before_customer_login_form' ); ?>
 
 <div class="u-columns col2-set row" id="customer_login">
 
-	<div class="u-column1 col-1 col-md-6">
+	<div class="u-column1 col-12 col-md-6">
 
 <?php endif; ?>
 
