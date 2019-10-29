90bbcfe29eb4428454f2dc99a5ca83ba8b839284
UnderstrapFramework <54932057+UnderstrapFramework@users.noreply.github.com>
2019-10-29 16:20:33 +0000

    Added wp-admin functions

--- functions.php
+++ functions.php
@@ -23,6 +23,7 @@ $understrap_includes = array(
 	'/class-wp-bootstrap-navwalker.php',    // Load custom WordPress nav walker.
 	'/woocommerce.php',                     // Load WooCommerce functions.
 	'/editor.php',                          // Load Editor functions.
+	'/wp-admin.php',                        // /wp-admin/ related functions
 	'/deprecated.php',                      // Load deprecated functions.
 );
 
