27824b3c9cd5b19a2d7d6f2a7f2ccd35989789c4
IanDelMar <42134098+IanDelMar@users.noreply.github.com>
2019-11-12 08:51:05 +0100

    Add aria-label to <nav>

--- header.php
+++ header.php
@@ -30,7 +30,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 		<a class="skip-link sr-only sr-only-focusable" href="#content"><?php esc_html_e( 'Skip to content', 'understrap' ); ?></a>
 
-		<nav class="navbar navbar-expand-md navbar-dark bg-primary">
+		<nav class="navbar navbar-expand-md navbar-dark bg-primary" aria-label="<?php echo esc_attr_x( 'Main', 'navigation', 'understrap' ); ?>">
 
 		<?php if ( 'container' == $container ) : ?>
 			<div class="container">
