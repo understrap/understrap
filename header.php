c636a000ec5aabe8ba91d1684fbae08dfb486136
IanDelMar <42134098+IanDelMar@users.noreply.github.com>
2019-12-03 09:38:02 +0100

    Add id to nav

--- header.php
+++ header.php
@@ -30,7 +30,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 		<a class="skip-link sr-only sr-only-focusable" href="#content"><?php esc_html_e( 'Skip to content', 'understrap' ); ?></a>
 
-		<nav class="navbar navbar-expand-md navbar-dark bg-primary">
+		<nav id="main-nav" class="navbar navbar-expand-md navbar-dark bg-primary">
 
 		<?php if ( 'container' == $container ) : ?>
 			<div class="container">
