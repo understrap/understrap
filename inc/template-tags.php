430936e7acfb6cb05eeae6ff9179e4a1ce91a306
IanDelMar <42134098+IanDelMar@users.noreply.github.com>
2019-12-27 08:51:04 +0100

    Remove translations
    Should not be up to the translator to change HTML.

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -159,11 +159,10 @@ if ( ! function_exists( 'understrap_body_attributes' ) ) {
 	}
 }
 
-/**
- * Display navigation to next/previous post when applicable.
- */
-
 if ( ! function_exists( 'understrap_post_nav' ) ) {
+	/**
+	 * Display navigation to next/previous post when applicable.
+	 */
 	function understrap_post_nav() {
 		// Don't print empty markup if there's nowhere to navigate.
 		$previous = ( is_attachment() ) ? get_post( get_post()->post_parent ) : get_adjacent_post( false, '', true );
@@ -178,10 +177,10 @@ if ( ! function_exists( 'understrap_post_nav' ) ) {
 			<div class="row nav-links justify-content-between">
 				<?php
 				if ( get_previous_post_link() ) {
-					previous_post_link( '<span class="nav-previous">%link</span>', _x( '<i class="fa fa-angle-left"></i>&nbsp;%title', 'Previous post link', 'understrap' ) );
+					previous_post_link( '<span class="nav-previous">%link</span>', '<i class="fa fa-angle-left"></i>&nbsp;%title' );
 				}
 				if ( get_next_post_link() ) {
-					next_post_link( '<span class="nav-next">%link</span>', _x( '%title&nbsp;<i class="fa fa-angle-right"></i>', 'Next post link', 'understrap' ) );
+					next_post_link( '<span class="nav-next">%link</span>', '%title&nbsp;<i class="fa fa-angle-right"></i>' );
 				}
 				?>
 			</div><!-- .nav-links -->
