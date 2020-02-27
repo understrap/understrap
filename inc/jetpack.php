02e7c5b8f17d8ec0ec5731e155018f8051de9c82
Amanda Dominy <gillespie.amanda@gmail.com>
2020-02-27 17:53:25 +0000

    Improvements to Jetpack Infinite Scroll support
    Added the id of the sidebar "footerfull" to the infinite-scroll argument list, so that if there are any widgets in the footer, the default Infinite Scroll behaviour would change to on-click instead.
    
    Added some scss to make the default layout of the Infinite Scroll button and footer match Bootstrap 4.

--- inc/jetpack.php
+++ inc/jetpack.php
@@ -3,7 +3,6 @@
  * Jetpack Compatibility File
  *
  * @link https://jetpack.me/
- *
  * @package UnderStrap
  */
 
@@ -13,30 +12,40 @@ defined( 'ABSPATH' ) || exit;
 /**
  * Jetpack setup function.
  *
- * See: https://jetpack.me/support/infinite-scroll/
- * See: https://jetpack.me/support/responsive-videos/
+ * Add theme support for a variety of Jetpack features, and assign their
+ * callback functions.
+ *
+ * @uses add_theme_support
+ * @link https://jetpack.com/support/infinite-scroll/
+ * @link https://jetpack.com/support/responsive-videos/
+ * @return void
  */
 
 add_action( 'after_setup_theme', 'understrap_components_jetpack_setup' );
 
 if ( ! function_exists( 'understrap_components_jetpack_setup' ) ) {
 	function understrap_components_jetpack_setup() {
-		// Add theme support for Infinite Scroll.
+		/**
+		 * Add theme support for Infinite Scroll.
+		 *
+		 * Pulls the next posts automatically into view when the reader
+		 * approaches the bottom of the page.
+		 */
 		add_theme_support(
 			'infinite-scroll',
 			array(
-				'container' => 'main',
-				'render'    => 'understrap_components_infinite_scroll_render',
-				'footer'    => 'page',
+				'container'      => 'main', // the ID of the HTML element to add additional posts to.
+				'footer'         => 'page', // jetpack will add a footer matching the width of `page`.
+				'footer_widgets' => 'footerfull', // ID of the sidebar containing footer widgets.
+				'render'         => 'understrap_components_infinite_scroll_render',
 			)
 		);
 
 		// Add theme support for Responsive Videos.
 		add_theme_support( 'jetpack-responsive-videos' );
 
-		// Add theme support for Social Menus
+		// Add theme support for Social Menus.
 		add_theme_support( 'jetpack-social-menu' );
-
 	}
 }
 
