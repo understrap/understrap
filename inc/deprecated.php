5be575c83be5a72f30f318951ce45a1540d525de
IanDelMar <42134098+IanDelMar@users.noreply.github.com>
2020-04-17 10:11:22 +0200

    Deprecate understrap_adjust_body_class()
    As of v4 Alpha 6 Bootstrap no longer styles the .tag CSS class.
    
    See shiplist: https://github.com/twbs/bootstrap/issues/20939

--- inc/deprecated.php
+++ inc/deprecated.php
@@ -8,6 +8,28 @@
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
 
+if ( ! function_exists( 'understrap_adjust_body_class' ) ) {
+	/**
+	 * Setup body classes.
+	 *
+	 * @param string $classes CSS classes.
+	 *
+	 * @deprecated 0.9.4 Styling of tag has been removed in Bootstrap v4 Alpha 6.
+	 * @link https://github.com/twbs/bootstrap/issues/20939
+	 */
+	function understrap_adjust_body_class( $classes ) {
+
+		foreach ( $classes as $key => $value ) {
+			if ( 'tag' == $value ) {
+				unset( $classes[ $key ] );
+			}
+		}
+
+		return $classes;
+
+	}
+}
+
 if ( ! function_exists( 'understrap_slbd_count_widgets' ) ) {
 	/**
 	 * Count number of widgets in a sidebar
