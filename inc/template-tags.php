4e4c0089b18bb416a6bd1c36bfda2055899af5d6
IanDelMar <42134098+IanDelMar@users.noreply.github.com>
2020-04-22 07:45:01 +0200

    Switch from FA to Bootstrap Icons in understrap_post_nav()
    Uses Bootstrap Icons instead of Font Awesome. Font Awesome 4 is no longer a dependency of UnderStrap. Child themes may use the understrap_bootstrap_icons hook to add additional Bootstrap Icons and may use the understrap_bootstrap_icon_svg_atts hook to adjust the svg attributes.
    
    understrap_get_allowed_html_svg() gives allowed tags and attributes for svgs. This may be altered by using the understrap_allowed_html_svg hook.

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -175,14 +175,147 @@ if ( ! function_exists( 'understrap_post_nav' ) ) {
 		<nav class="nav navigation post-navigation justify-content-between" aria-labelledby="post-nav-label">
 			<h2 id="post-nav-label" class="sr-only"><?php esc_html_e( 'Post navigation', 'understrap' ); ?></h2>
 			<?php
+			$icon_atts = array(
+				'height' => '0.65rem',
+				'width'  => '0.65rem',
+			);
 			if ( get_previous_post_link() ) {
-				previous_post_link( '%link', '<i class="fa fa-angle-left" aria-hidden="true"></i>&nbsp;%title' );
+				previous_post_link( '%link', understrap_bootstrap_icon( 'chevron-left', $icon_atts, false ) . '&nbsp;%title' );
 			}
 			if ( get_next_post_link() ) {
-				next_post_link( '%link', '%title&nbsp;<i class="fa fa-angle-right" aria-hidden="true"></i>' );
+				next_post_link( '%link', '%title&nbsp;' . understrap_bootstrap_icon( 'chevron-right', $icon_atts, false ) );
 			}
 			?>
 		</nav><!-- .navigation -->
 		<?php
 	}
 }
+
+if ( ! function_exists( 'understrap_bootstrap_icon' ) ) {
+	/**
+	 * Retrieves or diplays HTML markup for a Bootstrap icon.
+	 *
+	 * @param string   $name Name of the Bootstrap icon as used in the svg class: 'bi bi-name'.
+	 * @param string[] $args (Optional) Associative array of attribute => value pairs for the <svg> tag.
+	 * @param bool     $echo (Optional) Whether to echo (true) or return (return) the svg. Default: true.
+	 * @return string
+	 */
+	function understrap_bootstrap_icon( $name, $args = array(), $echo = true ) {
+
+		// Bootstrap Icons as of v1.0.0-alpha3.
+		$icons = array(
+			'chevron-right' => '<path fill-rule="evenodd" d="M6.646 3.646a.5.5 0 01.708 0l6 6a.5.5 0 010 .708l-6 6a.5.5 0 01-.708-.708L12.293 10 6.646 4.354a.5.5 0 010-.708z" clip-rule="evenodd"/>',
+			'chevron-left'  => '<path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 010 .708L5.707 8l5.647 5.646a.5.5 0 01-.708.708l-6-6a.5.5 0 010-.708l6-6a.5.5 0 01.708 0z" clip-rule="evenodd"/>',
+		);
+
+		/**
+		 * Filters the available Bootstrap icons.
+		 *
+		 * @param string[] $icons Associative array of the Bootstrap icons' HTML excluding the <svg> tag.
+		 */
+		$icons = apply_filters( 'understrap_bootstrap_icons', $icons );
+
+		// Return early if icon does not exist.
+		if ( ! isset( $icons[ $name ] ) ) {
+			return;
+		}
+
+		$default_svg_atts = array(
+			'width'       => '1em',
+			'height'      => '1em',
+			'viewBox'     => '0 0 16 16',
+			'fill'        => 'currentColor',
+			'xmlns'       => 'http://www.w3.org/2000/svg',
+			'focusable'   => 'false',
+			'role'        => 'img',
+			'aria-hidden' => 'true',
+		);
+
+		/**
+		 * Filters the default arguments for Bootstrap icons.
+		 *
+		 * @param string[] $default_svg_atts Associative array of attribute => value pairs for the <svg> tag.
+		 */
+		$default_svg_atts = apply_filters( 'understrap_bootstrap_icon_svg_atts', $default_svg_atts );
+
+		$custom_svg_atts = ! empty( $args ) ? true : false;
+		$atts            = $custom_svg_atts ? wp_parse_args( $args, $default_svg_atts ) : $default_svg_atts;
+
+		if ( isset( $atts['class'] ) ) {
+			$class = ' ' . $atts['class'];
+			unset( $atts['class'] );
+		} else {
+			$class = '';
+		}
+
+		$svg_atts = '';
+		foreach ( $atts as $att => $value ) {
+			switch ( strtolower( $att ) ) {
+				case 'xmlns':
+					$svg_atts .= $att . '="' . esc_url( $value ) . '" ';
+					break;
+				case 'viewbox':
+					$svg_atts .= $att . '="' . $value . '" ';
+					break;
+				default:
+					$svg_atts .= $att . '="' . esc_attr( $value ) . '" ';
+			}
+		}
+		rtrim( $svg_atts );
+
+		$svg = '<svg class="bi bi-' . $name . $class . '" ' . $svg_atts . '>' . $icons[ $name ] . '</svg>';
+		if ( (bool) $echo ) {
+			echo wp_kses( $svg, understrap_allowed_html_svg() );
+		} else {
+			return $svg;
+		}
+	}
+} // End of if function_exists( 'understrap_bootstrap_icon' ).
+
+if ( ! function_exists( 'understrap_get_allowed_html_svg' ) ) {
+	/**
+	 * Retrieves allowed HTML tags and attributes for <svg> tags.
+	 *
+	 * @return array|string[]
+	 */
+	function understrap_get_allowed_html_svg() {
+		$allowed_html = array(
+			'svg'     => array(
+				'class'       => true,
+				'xmlns'       => true,
+				'width'       => true,
+				'height'      => true,
+				'viewbox'     => true,
+				'aria-hidden' => true,
+				'role'        => true,
+				'focusable'   => true,
+			),
+			'path'    => array(
+				'fill'      => true,
+				'fill-rule' => true,
+				'clip-rule' => true,
+				'd'         => true,
+				'transform' => true,
+			),
+			'polygon' => array(
+				'fill'      => true,
+				'fill-rule' => true,
+				'points'    => true,
+				'transform' => true,
+				'focusable' => true,
+			),
+		);
+
+		/**
+		 * Filters the allowed html for svg markup.
+		 *
+		 * @param string[] $allowed_html Associative array of allowed HTML tags and attributes.
+		 */
+		$allowed_html = apply_filters( 'understrap_allowed_html_svg', $allowed_html );
+		if ( ! is_array( $allowed_html ) ) {
+			return array();
+		}
+
+		return $allowed_html;
+	}
+} // End of if function_exists( 'understrap_get_allowed_html_svg' ).
