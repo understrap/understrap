d601c276fc68a4ed73b55f2919a7313d16db2e31
Daniel <danny@late3.co.uk>
2020-04-26 19:01:31 +0100

    Update class-wp-bootstrap-navwalker.php
    Give the top-level menu item clickable link rather than #

--- inc/class-wp-bootstrap-navwalker.php
+++ inc/class-wp-bootstrap-navwalker.php
@@ -190,7 +190,7 @@ if ( ! class_exists( 'Understrap_WP_Bootstrap_Navwalker' ) ) {
 
 			// If item has_children add atts to <a>.
 			if ( isset( $args->has_children ) && $args->has_children && 0 === $depth && 1 !== $args->depth ) {
-				$atts['href']          = '#';
+				$atts['href']          = ( $item->url );
 				$atts['data-toggle']   = 'dropdown';
 				$atts['aria-haspopup'] = 'true';
 				$atts['aria-expanded'] = 'false';
