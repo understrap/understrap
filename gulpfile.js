7ea00a95c9087a9a6b965d427fceb0270e8802d7
IanDelMar <42134098+IanDelMar@users.noreply.github.com>
2020-04-28 15:03:27 +0200

    Remove Bower
    Bower has been dropped a while ago

--- gulpfile.js
+++ gulpfile.js
@@ -171,7 +171,7 @@ gulp.task('watch-bs', gulp.parallel('browser-sync', 'watch'));
 
 // Run:
 // gulp copy-assets.
-// Copy all needed dependency assets files from bower_component assets to themes /js, /scss and /fonts folder. Run this task after bower install or bower update
+// Copy all needed dependency assets files from node_modules to theme's /js, /scss and /fonts folder. Run this task after npm install or npm update
 
 ////////////////// All Bootstrap SASS  Assets /////////////////////////
 gulp.task('copy-assets', function (done) {
@@ -299,8 +299,6 @@ gulp.task(
 		return gulp
 			.src([
 				'**/*',
-				`!${paths.bower}`,
-				`!${paths.bower}/**`,
 				`!${paths.node}`,
 				`!${paths.node}/**`,
 				`!${paths.dist}`,
