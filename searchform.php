ae30c0e2515b71a18020f5f4943184ef3343a337
IanDelMar <42134098+IanDelMar@users.noreply.github.com>
2021-07-24 16:51:25 +0200

    Add unique id to search form inputs

--- searchform.php
+++ searchform.php
@@ -7,15 +7,17 @@
 
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
+
+$uid = wp_unique_id( '-' ); // The search form specific unique ID.
 ?>
 
-<form method="get" class="searchform" id="<?php wp_unique_id( 'searchform-' ); ?>" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
-	<label class="sr-only" for="s"><?php esc_html_e( 'Search', 'understrap' ); ?></label>
+<form method="get" class="search-form" id="<?php echo 'search-form' . $uid; ?>" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
+	<label class="sr-only" for="<?php echo 's' . $uid; ?>"><?php esc_html_e( 'Search', 'understrap' ); ?></label>
 	<div class="input-group">
-		<input class="field form-control" id="s" name="s" type="text"
+		<input class="field form-control" id="<?php echo 's' . $uid; ?>" name="s" type="text"
 			placeholder="<?php esc_attr_e( 'Search &hellip;', 'understrap' ); ?>" value="<?php the_search_query(); ?>">
 		<span class="input-group-append">
-			<input class="submit btn btn-primary" id="searchsubmit" name="submit" type="submit"
+			<input class="submit btn btn-primary" id="<?php echo 'search-submit' . $uid; ?>" name="submit" type="submit"
 			value="<?php esc_attr_e( 'Search', 'understrap' ); ?>">
 		</span>
 	</div>
