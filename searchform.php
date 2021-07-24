06545258e07e4e41514bf72a8500fcc5d5dbf656
IanDelMar <42134098+IanDelMar@users.noreply.github.com>
2021-07-24 17:04:11 +0200

    Add aria-label to search form
    The `$args` array parameter and its `aria_label` key were added in WP 5.2.0 and replaced the `$echo` boolean flag.

--- searchform.php
+++ searchform.php
@@ -9,9 +9,14 @@
 defined( 'ABSPATH' ) || exit;
 
 $uid = wp_unique_id( '-' ); // The search form specific unique ID.
+
+$aria_label = '';
+if ( isset( $args['aria_label'] ) && ! empty( $args['aria_label'] ) ) {
+	$aria_label = 'aria-label="' . esc_attr( $args['aria_label'] ) . '"';
+}
 ?>
 
-<form method="get" class="search-form" id="<?php echo 'search-form' . $uid; ?>" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
+<form method="get" class="search-form" id="<?php echo 'search-form' . $uid; ?>" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search" <?php echo $aria_label; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- Escaped above. ?>>
 	<label class="sr-only" for="<?php echo 's' . $uid; ?>"><?php esc_html_e( 'Search', 'understrap' ); ?></label>
 	<div class="input-group">
 		<input class="field form-control" id="<?php echo 's' . $uid; ?>" name="s" type="text"
@@ -22,3 +27,4 @@ $uid = wp_unique_id( '-' ); // The search form specific unique ID.
 		</span>
 	</div>
 </form>
+<?php
