3bbd2115819a7ce5cac27b5c92f0f52bc58fee68
IanDelMar <42134098+IanDelMar@users.noreply.github.com>
2021-07-24 17:58:21 +0200

    Add 'WordPress' classes to search form inputs

--- searchform.php
+++ searchform.php
@@ -19,10 +19,10 @@ if ( isset( $args['aria_label'] ) && ! empty( $args['aria_label'] ) ) {
 <form role="search" class="search-form" id="<?php echo 'search-form' . $uid; ?>" method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>" <?php echo $aria_label; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- Escaped above. ?>>
 	<label class="sr-only" for="<?php echo 's' . $uid; ?>"><?php esc_html_e( 'Search', 'understrap' ); ?></label>
 	<div class="input-group">
-		<input type="search" class="field form-control" id="<?php echo 's' . $uid; ?>" name="s" value="<?php the_search_query(); ?>"
+		<input type="search" class="field search-field form-control" id="<?php echo 's' . $uid; ?>" name="s" value="<?php the_search_query(); ?>"
 			placeholder="<?php esc_attr_e( 'Search &hellip;', 'understrap' ); ?>">
 		<span class="input-group-append">
-			<input type="submit" class="submit btn btn-primary" id="<?php echo 'search-submit' . $uid; ?>" name="submit"
+			<input type="submit" class="submit search-submit btn btn-primary" id="<?php echo 'search-submit' . $uid; ?>" name="submit"
 			value="<?php esc_attr_e( 'Search', 'understrap' ); ?>">
 		</span>
 	</div>
