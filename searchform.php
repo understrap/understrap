5ebbcdeff5586461b7d8dadf8a50acd91d1b3ffe
IanDelMar <42134098+IanDelMar@users.noreply.github.com>
2021-07-26 03:55:25 +0200

    Wrap form elements in div with role="search"

--- searchform.php
+++ searchform.php
@@ -16,13 +16,15 @@ if ( isset( $args['aria_label'] ) && ! empty( $args['aria_label'] ) ) {
 }
 ?>
 
-<form role="search" class="search-form" method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>" <?php echo $aria_label; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- Escaped above. ?>>
-	<label class="sr-only" for="<?php echo $uid; ?>"><?php echo esc_html_x( 'Search for:', 'label', 'understrap' ); ?></label>
-	<div class="input-group">
-		<input type="search" class="field search-field form-control" id="<?php echo $uid; ?>" name="s" value="<?php the_search_query(); ?>" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'understrap' ); ?>">
-		<span class="input-group-append">
-			<input type="submit" class="submit search-submit btn btn-primary" name="submit" value="<?php echo esc_attr_x( 'Search', 'submit button', 'understrap' ); ?>">
-		</span>
+<form class="search-form" method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>" <?php echo $aria_label; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- Escaped above. ?>>
+	<div role="search">
+		<label class="sr-only" for="<?php echo $uid; ?>"><?php echo esc_html_x( 'Search for:', 'label', 'understrap' ); ?></label>
+		<div class="input-group">
+			<input type="search" class="field search-field form-control" id="<?php echo $uid; ?>" name="s" value="<?php the_search_query(); ?>" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'understrap' ); ?>">
+			<span class="input-group-append">
+				<input type="submit" class="submit search-submit btn btn-primary" name="submit" value="<?php echo esc_attr_x( 'Search', 'submit button', 'understrap' ); ?>">
+			</span>
+		</div>
 	</div>
 </form>
 <?php
