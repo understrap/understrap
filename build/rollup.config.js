91ac63dcb609dd4c8a77435eb64c1004160bbbae
Brian Coords <bacoords@gmail.com>
2021-08-19 08:09:02 -0700

    fixes rollup bundling

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -3,17 +3,20 @@
 const path = require('path')
 const { babel } = require('@rollup/plugin-babel')
 const { nodeResolve } = require('@rollup/plugin-node-resolve')
+import multi from '@rollup/plugin-multi-entry'
 const banner = require('./banner.js')
 
 let fileDest = 'theme.js'
-const external = ['jquery', 'popper.js']
+const external = ['jquery']
 const plugins = [
   babel({
     // Only transpile our source code
     exclude: 'node_modules/**',
     // Include the helpers in the bundle, at most one copy of each
     babelHelpers: 'bundled'
-  })
+  }),
+  nodeResolve(),
+  multi()
 ]
 const globals = {
   jquery: 'jQuery', // Ensure we use jQuery which is always available even in noConflict mode
@@ -21,15 +24,8 @@ const globals = {
 }
 
 
-fileDest = 'theme.js'
-// Remove last entry in external array to bundle Popper
-external.pop()
-delete globals['popper.js']
-plugins.push(nodeResolve())
-
-
 module.exports = {
-  input: path.resolve(__dirname, '../src/js/custom-javascript.js'),
+  input: [path.resolve(__dirname, '../src/js/bootstrap.js'), path.resolve(__dirname, '../src/js/skip-link-focus-fix.js'), path.resolve(__dirname, '../src/js/custom-javascript.js')],
   output: {
     banner,
     file: path.resolve(__dirname, `../js/${fileDest}`),
