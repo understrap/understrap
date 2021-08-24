3ede2eaf997c9266fb86e0bbb993838fa8eed260
Brian Coords <bacoords@gmail.com>
2021-08-24 13:04:01 -0700

    adds copy assets task

---
+++ src/build/copy-assets.js
@@ -0,0 +1,21 @@
+const { promises: fs } = require("fs")
+const path = require("path")
+
+async function copyDir(src, dest) {
+    await fs.mkdir(dest, { recursive: true });
+    let entries = await fs.readdir(src, { withFileTypes: true });
+
+    for (let entry of entries) {
+        let srcPath = path.join(src, entry.name);
+        let destPath = path.join(dest, entry.name);
+
+        entry.isDirectory() ?
+            await copyDir(srcPath, destPath) :
+            await fs.copyFile(srcPath, destPath);
+    }
+}
+
+// Copy all Bootstrap SCSS files.
+copyDir('./node_modules/bootstrap/scss', './src/sass/assets/bootstrap4');
+// Copy all Font Awesome SCSS files.
+copyDir('./node_modules/font-awesome/scss', './src/sass/assets/fontawesome');
