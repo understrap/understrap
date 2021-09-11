01bc6fe7258621eb1a4977d478050898907ff268
Brian Coords <bacoords@gmail.com>
2021-09-11 16:36:56 -0700

    updates config file to generate correct json palette

--- src/build-bootstrap4/postcss.config.js
+++ src/build-bootstrap4/postcss.config.js
@@ -10,22 +10,22 @@ module.exports = ctx => {
     plugins: {
       autoprefixer: {
         cascade: false
-      },
+	  },
 	  "postcss-understrap-palette-generator" : {
 		colors: [
-			"--bs-blue",
-			"--bs-indigo",
-			"--bs-purple",
-			"--bs-pink",
-			"--bs-red",
-			"--bs-orange",
-			"--bs-yellow",
-			"--bs-green",
-			"--bs-teal",
-			"--bs-cyan",
-			"--bs-white",
-			"--bs-gray",
-			"--bs-gray-dark"
+			"--blue",
+			"--indigo",
+			"--purple",
+			"--pink",
+			"--red",
+			"--orange",
+			"--yellow",
+			"--green",
+			"--teal",
+			"--cyan",
+			"--white",
+			"--gray",
+			"--gray-dark"
 		]
 	  }
     }
--- src/build-bootstrap4/rollup.config.js
+++ src/build-bootstrap4/rollup.config.js
@@ -4,7 +4,7 @@ const path = require('path')
 const { babel } = require('@rollup/plugin-babel')
 const { nodeResolve } = require('@rollup/plugin-node-resolve')
 import multi from '@rollup/plugin-multi-entry'
-const banner = require('./banner.js')
+const banner = require('../build/banner.js')
 
 let fileDest = 'theme-bootstrap4.js'
 const external = ['jquery']
