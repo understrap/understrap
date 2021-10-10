5f416a5a9bba36a4a3835ed47b99e21d8b03f999
Gabriel Castillo <gabriel@gabrielcastillo.net>
2021-10-10 01:28:44 -0700

    add footer role to sidebar-footerfull

--- sidebar-templates/sidebar-footerfull.php
+++ sidebar-templates/sidebar-footerfull.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Sidebar setup for footer full
  *
@@ -6,23 +7,23 @@
  */
 
 // Exit if accessed directly.
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 
-$container = get_theme_mod( 'understrap_container_type' );
+$container = get_theme_mod('understrap_container_type');
 
 ?>
 
-<?php if ( is_active_sidebar( 'footerfull' ) ) : ?>
+<?php if (is_active_sidebar('footerfull')) : ?>
 
 	<!-- ******************* The Footer Full-width Widget Area ******************* -->
 
-	<div class="wrapper" id="wrapper-footer-full">
+	<div class="wrapper" id="wrapper-footer-full" role="footer">
 
-		<div class="<?php echo esc_attr( $container ); ?>" id="footer-full-content" tabindex="-1">
+		<div class="<?php echo esc_attr($container); ?>" id="footer-full-content" tabindex="-1">
 
 			<div class="row">
 
-				<?php dynamic_sidebar( 'footerfull' ); ?>
+				<?php dynamic_sidebar('footerfull'); ?>
 
 			</div>
 
@@ -30,5 +31,5 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 	</div><!-- #wrapper-footer-full -->
 
-	<?php
+<?php
 endif;
