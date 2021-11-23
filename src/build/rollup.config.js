de40cb1910834a2785f1d713d364b108da40202a
Brian Coords <bacoords@gmail.com>
2021-11-22 16:03:10 -0800

    fixes bootstrap 5 bundling

--- src/build/rollup.config.js
+++ src/build/rollup.config.js
@@ -3,12 +3,13 @@
 const path = require('path')
 const { babel } = require('@rollup/plugin-babel')
 const { nodeResolve } = require('@rollup/plugin-node-resolve')
-import commonjs from "@rollup/plugin-commonjs";
+import commonjs from '@rollup/plugin-commonjs'
 import multi from '@rollup/plugin-multi-entry'
+const replace = require('@rollup/plugin-replace')
 const banner = require('./banner.js')
 
 let fileDest = 'theme.js'
-const external = ['jquery', '@popperjs/core']
+const external = ['jquery']
 const plugins = [
   babel({
     // Only transpile our source code
@@ -16,13 +17,16 @@ const plugins = [
     // Include the helpers in the bundle, at most one copy of each
     babelHelpers: 'bundled'
   }),
+  replace({
+      'process.env.NODE_ENV': '"production"',
+      preventAssignment: true
+  }),
   nodeResolve(),
   commonjs(),
   multi()
 ]
 const globals = {
   jquery: 'jQuery', // Ensure we use jQuery which is always available even in noConflict mode
-  '@popperjs/core': 'Popper'
 }
 
 
