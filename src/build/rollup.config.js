151dd5ed6b44f4357742446214dca8e74af0daeb
IanDelMar <42134098+IanDelMar@users.noreply.github.com>
2022-02-28 11:12:05 +0100

    merge bs4 and bs5 rollup config

--- src/build/rollup.config.js
+++ src/build/rollup.config.js
@@ -1,19 +1,46 @@
 'use strict';
 
+/**
+ * External dependencies
+ */
 const path = require( 'path' );
 const { babel } = require( '@rollup/plugin-babel' );
 const { nodeResolve } = require( '@rollup/plugin-node-resolve' );
 const commonjs = require( '@rollup/plugin-commonjs' );
 const multi = require( '@rollup/plugin-multi-entry' );
 const replace = require( '@rollup/plugin-replace' );
+
+/**
+ * Internal dependencies
+ */
 const banner = require( './banner.js' );
 
-const fileDest = 'theme.js';
+// Determine if we want to build for Bootstrap v4 or v5.
+const BS4 = process.argv[ process.argv.length - 1 ] === 'BS4';
+
+// Populate Bootstrap version specific variables.
+let bsVersion = 5;
+let bsSrcFile = 'bootstrap.js';
+let fileDest = 'theme.js';
+let globals = {
+	jquery: 'jQuery', // Ensure we use jQuery which is always available even in noConflict mode
+	'@popperjs/core': 'Popper',
+};
+if ( BS4 ) {
+	// Adjustmets for Bootstrap version 4.
+	bsVersion = 4;
+	bsSrcFile = 'bootstrap4.js';
+	fileDest = 'theme-bootstrap4.js';
+	delete globals[ '@popperjs/core' ];
+	Object.assign( globals, { 'popper.js': 'Popper' } );
+}
+
 const external = [ 'jquery' ];
+
 const plugins = [
 	babel( {
-		browserslistEnv: 'bs5',
-		// Include the helpers in the bundle, at most one copy of each
+		browserslistEnv: `bs${ bsVersion }`,
+		// Include the helpers in the bundle, at most one copy of each.
 		babelHelpers: 'bundled',
 	} ),
 	replace( {
@@ -24,14 +51,10 @@ const plugins = [
 	commonjs(),
 	multi(),
 ];
-const globals = {
-	jquery: 'jQuery', // Ensure we use jQuery which is always available even in noConflict mode
-	'@popperjs/core': 'Popper',
-};
 
 module.exports = {
 	input: [
-		path.resolve( __dirname, '../js/bootstrap.js' ),
+		path.resolve( __dirname, `../js/${ bsSrcFile }` ),
 		path.resolve( __dirname, '../js/skip-link-focus-fix.js' ),
 		path.resolve( __dirname, '../js/custom-javascript.js' ),
 	],
