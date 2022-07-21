b0783440d319bfe32d1d50a385dff282af7b9431
IanDelMar <42134098+IanDelMar@users.noreply.github.com>
2022-07-21 03:12:55 +0200

    do not use inner functions

--- inc/customizer.php
+++ inc/customizer.php
@@ -41,26 +41,6 @@ if ( ! function_exists( 'understrap_theme_customize_register' ) ) {
 			)
 		);
 
-		/**
-		 * Select sanitization function
-		 *
-		 * @param string               $input   Slug to sanitize.
-		 * @param WP_Customize_Setting $setting Setting instance.
-		 * @return string Sanitized slug if it is a valid choice; otherwise, the setting default.
-		 */
-		function understrap_theme_slug_sanitize_select( $input, $setting ) {
-
-			// Ensure input is a slug (lowercase alphanumeric characters, dashes and underscores are allowed only).
-			$input = sanitize_key( $input );
-
-			// Get the list of possible select options.
-			$choices = $setting->manager->get_control( $setting->id )->choices;
-
-			// If the input is a valid key, return it; otherwise, return the default.
-			return ( array_key_exists( $input, $choices ) ? $input : $setting->default );
-
-		}
-
 		$wp_customize->add_setting(
 			'understrap_bootstrap_version',
 			array(
@@ -215,6 +195,28 @@ if ( ! function_exists( 'understrap_theme_customize_register' ) ) {
 	}
 } // End of if function_exists( 'understrap_theme_customize_register' ).
 add_action( 'customize_register', 'understrap_theme_customize_register' );
+
+if ( ! function_exists( 'understrap_theme_slug_sanitize_select') ) {
+	/**
+	 * Sanitize select.
+	 *
+	 * @param string               $input   Slug to sanitize.
+	 * @param WP_Customize_Setting $setting Setting instance.
+	 * @return string Sanitized slug if it is a valid choice; otherwise, the setting default.
+	 */
+	function understrap_theme_slug_sanitize_select( $input, $setting ) {
+
+		// Ensure input is a slug (lowercase alphanumeric characters, dashes and underscores are allowed only).
+		$input = sanitize_key( $input );
+
+		// Get the list of possible select options.
+		$choices = $setting->manager->get_control( $setting->id )->choices;
+
+		// If the input is a valid key, return it; otherwise, return the default.
+		return ( array_key_exists( $input, $choices ) ? $input : $setting->default );
+
+	}
+}
 
 /**
  * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
