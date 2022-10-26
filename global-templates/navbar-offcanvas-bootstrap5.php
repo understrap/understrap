274d8d22167521e9e9ab5bcfa88ad7d1df486e72
IanDelMar <42134098+IanDelMar@users.noreply.github.com>
2022-10-26 11:39:58 +0200

    make aria label translatable

--- global-templates/navbar-offcanvas-bootstrap5.php
+++ global-templates/navbar-offcanvas-bootstrap5.php
@@ -23,14 +23,27 @@ $container = get_theme_mod( 'understrap_container_type' );
 		<!-- Your site branding in the menu -->
 		<?php get_template_part( 'global-templates/navbar-branding' ); ?>
 
-		<button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#navbarNavOffcanvas" aria-controls="navbarNavOffcanvas" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'understrap' ); ?>">
+		<button
+			class="navbar-toggler"
+			type="button"
+			data-bs-toggle="offcanvas"
+			data-bs-target="#navbarNavOffcanvas"
+			aria-controls="navbarNavOffcanvas"
+			aria-expanded="false"
+			aria-label="<?php esc_attr_e( 'Open menu', 'understrap' ); ?>"
+		>
 			<span class="navbar-toggler-icon"></span>
 		</button>
 
 		<div class="offcanvas offcanvas-end bg-primary" tabindex="-1" id="navbarNavOffcanvas">
 
 			<div class="offcanvas-header justify-content-end">
-				<button type="button" class="btn-close btn-close-white text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+				<button
+					class="btn-close btn-close-white text-reset"
+					type="button"
+					data-bs-dismiss="offcanvas"
+					aria-label="<?php esc_attr_e( 'Close menu', 'understrap' ); ?>"
+				></button>
 			</div><!-- .offcancas-header -->
 
 			<!-- The WordPress Menu goes here -->
