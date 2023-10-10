9990385bf086038f7d3ab62738716861f04da56c
IanDelMar <42134098+IanDelMar@users.noreply.github.com>
2023-10-10 15:15:49 +0200

    trigger cs error

--- functions.php
+++ functions.php
@@ -43,3 +43,7 @@ if ( class_exists( 'Jetpack' ) ) {
 foreach ( $understrap_includes as $file ) {
 	require_once get_theme_file_path( $understrap_inc_dir . $file );
 }
+
+function trigger_cs_error( $string ) {
+	echo $string;
+}
