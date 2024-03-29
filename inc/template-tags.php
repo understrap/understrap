474b54194e75e16f6a6815f7dd32e129af8a7f00
IanDelMar <42134098+IanDelMar@users.noreply.github.com>
2024-03-29 15:25:06 +0100

    Refactor understrap_post_nav()

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -285,33 +285,30 @@ if ( ! function_exists( 'understrap_edit_post_link' ) ) {
 if ( ! function_exists( 'understrap_post_nav' ) ) {
 	/**
 	 * Displays the navigation to the next/previous post when applicable.
-	 *
-	 * @global WP_Post|null $post The current post.
 	 */
 	function understrap_post_nav() {
-		global $post;
-		if ( ! $post ) {
+		$previous_post_link = get_previous_post_link(
+			'<span class="nav-previous">%link</span>',
+			_x( '<i class="fa fa-angle-left"></i>&nbsp;%title', 'Previous post link', 'understrap' )
+		);
+
+		$next_post_link = get_next_post_link(
+			'<span class="nav-next">%link</span>',
+			_x( '%title&nbsp;<i class="fa fa-angle-right"></i>', 'Next post link', 'understrap' )
+		);
+
+		// Don't print empty markup if there's nowhere to navigate.
+		if ( '' === $previous_post_link && '' === $next_post_link ) {
 			return;
 		}
 
-		// Don't print empty markup if there's nowhere to navigate.
-		$previous = ( is_attachment() ) ? get_post( $post->post_parent ) : get_adjacent_post( false, '', true );
-		$next     = get_adjacent_post( false, '', false );
-		if ( ! $next && ! $previous ) {
-			return;
-		}
 		?>
 		<nav class="container navigation post-navigation">
 			<h2 class="screen-reader-text"><?php esc_html_e( 'Post navigation', 'understrap' ); ?></h2>
 			<div class="d-flex nav-links justify-content-between">
 				<?php
-				if ( get_previous_post_link() ) {
-					previous_post_link( '<span class="nav-previous">%link</span>', _x( '<i class="fa fa-angle-left"></i>&nbsp;%title', 'Previous post link', 'understrap' ) );
-				}
-
-				if ( get_next_post_link() ) {
-					next_post_link( '<span class="nav-next">%link</span>', _x( '%title&nbsp;<i class="fa fa-angle-right"></i>', 'Next post link', 'understrap' ) );
-				}
+				echo wp_kses_post( $previous_post_link );
+				echo wp_kses_post( $next_post_link );
 				?>
 			</div><!-- .nav-links -->
 		</nav><!-- .post-navigation -->
