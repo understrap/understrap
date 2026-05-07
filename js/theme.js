6ac8a677fc6a07f054b7c8929df1eba645b1ad68
baltaduygu31-prog <259948655+baltaduygu31-prog@users.noreply.github.com>
2026-05-07 23:52:26 +0000

    Rebuild CSS and JS files

--- js/theme.js
+++ js/theme.js
@@ -1,6 +1,6 @@
 /*!
  * Understrap v1.2.4 (https://understrap.com)
- * Copyright 2013-2024 The Understrap Authors (https://github.com/understrap/understrap/graphs/contributors)
+ * Copyright 2013-2026 The Understrap Authors (https://github.com/understrap/understrap/graphs/contributors)
  * Licensed under GPL-3.0 (https://www.gnu.org/licenses/gpl-3.0.html)
  */
 (function (global, factory) {
